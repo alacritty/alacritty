c8fd2c090c4732fb44d0d4250a2aa1b6722fde76
Joe Wilm <joe@jwilm.com>
2016-06-29 20:56:12 -0700

    Add license headers to source files

--- font/src/darwin/mod.rs
+++ font/src/darwin/mod.rs
@@ -1,3 +1,17 @@
+// Copyright 2016 Joe Wilm, The Alacritty Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 //! Font rendering based on CoreText
 //!
 //! TODO error handling... just search for unwrap.
