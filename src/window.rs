574586045f08307b310983ed713972d0b6a45c30
Joe Wilm <joe@jwilm.com>
2017-04-26 10:26:46 -0700

    Support setting _NET_WM_PID in X11 environments
    Support is added for setting _NET_WM_PID automatically. This is to
    support scripting of the window environment. For example, this makes it
    possible to script opening a window with same CWD:
    
    1. Retrieve the current window
    2. (new) get PID of window
    3. Check if it's Alacritty, find first child (presumably a shell), and
       get the child's cwd.
    4. Spawn new instance of terminal with cwd.
    
    Unaddressed in this commit is how this will coexist on a Wayland system.

--- src/window.rs
+++ src/window.rs
@@ -217,10 +217,14 @@ impl Window {
             window.make_current()?;
         }
 
-        Ok(Window {
+        let window = Window {
             glutin_window: window,
             cursor_visible: true,
-        })
+        };
+
+        window.run_os_extensions();
+
+        Ok(window)
     }
 
     /// Get some properties about the device
@@ -297,6 +301,50 @@ impl Window {
             self.cursor_visible = visible;
             self.glutin_window.set_cursor_state(if visible { glutin::CursorState::Normal }
                                                 else { glutin::CursorState::Hide }).unwrap();
+        }
+    }
+}
+
+pub trait OsExtensions {
+    fn run_os_extensions(&self) {}
+}
+
+#[cfg(not(any(target_os = "linux", target_os = "freebsd", target_os="dragonfly", target_os="openbsd")))]
+impl OsExtensions for Window { }
+
+#[cfg(any(target_os = "linux", target_os = "freebsd", target_os="dragonfly", target_os="openbsd"))]
+impl OsExtensions for Window {
+    fn run_os_extensions(&self) {
+        use ::glutin::os::unix::WindowExt;
+        use ::x11_dl::xlib::{self, XA_CARDINAL, PropModeReplace};
+        use ::std::ffi::{CStr};
+        use ::std::ptr;
+        use ::libc::getpid;
+
+        let xlib_display = self.glutin_window.get_xlib_display();
+        let xlib_window = self.glutin_window.get_xlib_window();
+
+        if let (Some(xlib_window), Some(xlib_display)) = (xlib_window, xlib_display) {
+            let xlib = xlib::Xlib::open().expect("get xlib");
+
+            // Set _NET_WM_PID to process pid
+            unsafe {
+                let _net_wm_pid = CStr::from_ptr(b"_NET_WM_PID\0".as_ptr() as *const _);
+                let atom = (xlib.XInternAtom)(xlib_display as *mut _, _net_wm_pid.as_ptr(), 0);
+                let pid = getpid();
+
+                (xlib.XChangeProperty)(xlib_display as _, xlib_window as _, atom,
+                    XA_CARDINAL, 32, PropModeReplace, &pid as *const i32 as *const u8, 1);
+
+            }
+            // Although this call doesn't actually pass any data, it does cause
+            // WM_CLIENT_MACHINE to be set. WM_CLIENT_MACHINE MUST be set if _NET_WM_PID is set
+            // (which we do above).
+            unsafe {
+                (xlib.XSetWMProperties)(xlib_display as _, xlib_window as _, ptr::null_mut(),
+                    ptr::null_mut(), ptr::null_mut(), 0, ptr::null_mut(), ptr::null_mut(),
+                    ptr::null_mut());
+            }
         }
     }
 }
