866668711c8feb6904b7f47a3f5e8458cdbf8500
Joe Wilm <joe@jwilm.com>
2017-08-22 10:54:46 -0700

    Fix newline + scroll region bug
    When a scroll region is active with the cursor below the bottom of the
    region, newlines should not cause the region to scroll.
    
    A ref test was added for this situation to prevent regressions.
    
    Thanks @hiciu for reporting and @nicm for the test case.
    
    Resolves #745.

--- src/term/mod.rs
+++ src/term/mod.rs
@@ -1405,7 +1405,7 @@ impl ansi::Handler for Term {
     #[inline]
     fn linefeed(&mut self) {
         trace!("linefeed");
-        if (self.cursor.point.line + 1) >= self.scroll_region.end {
+        if (self.cursor.point.line + 1) == self.scroll_region.end {
             self.scroll_up(Line(1));
         } else {
             self.cursor.point.line += 1;
