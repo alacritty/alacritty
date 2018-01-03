67682d54f8236fa22d220689cabdd616a4945e78
Christian Duerr <contact@christianduerr.com>
2018-01-03 02:07:20 +0100

    Add custom build matrix
    Added a custom build matrix so when `VERSION="nightly"` the nightly
    compiler is used and when `VERSION="stable"` the stable compiler is
    used.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $VERSION == "nightly" ]]; then
-    rustup run nightly cargo test --no-default-features --features "clippy"
+    cargo test --no-default-features --features "clippy"
 else
     cargo test --no-default-features
 fi
