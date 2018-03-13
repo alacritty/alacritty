alacritty/alacritty

commit 5dad4919a227a265880de3abaa9940b4220a2d95

Enable SRGB (#1178)
This will solve the error "Error creating GL context; Couldn't find any
pixel format that matches the criterias."

(I think this maybe a problem of mesa (18.0.0_rc4), but I'm not sure.

See the thread <https://mastodon.cardina1.red/@lo48576/99670278063669603>
for my debug log.

src/window.rs
@@ -200,6 +200,7 @@ impl Window {
             .with_transparency(true)
             .with_decorations(window_config.decorations());
         let context = ContextBuilder::new()
+            .with_srgb(true)
             .with_vsync(true);
         let window = ::glutin::GlWindow::new(window, context, &event_loop)?;
         window.show();

commit e045139c8586b0b0684c7785e20611c8f346dc34

Try to create window with different SRGB config when failed
This may truly solve #921 (and issue caused by #1178)
<https://github.com/jwilm/alacritty/issues/921#issuecomment-372619121>.

src/window.rs
@@ -183,6 +183,17 @@ impl From<glutin::ContextError> for Error {
     }
 }
 
+fn create_gl_window(
+    window: WindowBuilder,
+    event_loop: &EventsLoop,
+    srgb: bool,
+) -> ::std::result::Result<glutin::GlWindow, glutin::CreationError> {
+    let context = ContextBuilder::new()
+        .with_srgb(srgb)
+        .with_vsync(true);
+    ::glutin::GlWindow::new(window, context, event_loop)
+}
+
 impl Window {
     /// Create a new window
     ///
@@ -199,10 +210,8 @@ impl Window {
             .with_visibility(false)
             .with_transparency(true)
             .with_decorations(window_config.decorations());
-        let context = ContextBuilder::new()
-            .with_srgb(true)
-            .with_vsync(true);
-        let window = ::glutin::GlWindow::new(window, context, &event_loop)?;
+        let window = create_gl_window(window.clone(), &event_loop, false)
+            .or_else(|_| create_gl_window(window, &event_loop, true))?;
         window.show();
 
         // Text cursor