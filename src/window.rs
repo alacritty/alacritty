5dad4919a227a265880de3abaa9940b4220a2d95
らりお (YOSHIOKA Takuma) <lo48576@hard-wi.red>
2018-03-13 15:11:56 +0900

    Enable SRGB (#1178)
    This will solve the error "Error creating GL context; Couldn't find any
    pixel format that matches the criterias."
    
    (I think this maybe a problem of mesa (18.0.0_rc4), but I'm not sure.
    
    See the thread <https://mastodon.cardina1.red/@lo48576/99670278063669603>
    for my debug log.

--- src/window.rs
+++ src/window.rs
@@ -200,6 +200,7 @@ impl Window {
             .with_transparency(true)
             .with_decorations(window_config.decorations());
         let context = ContextBuilder::new()
+            .with_srgb(true)
             .with_vsync(true);
         let window = ::glutin::GlWindow::new(window, context, &event_loop)?;
         window.show();
