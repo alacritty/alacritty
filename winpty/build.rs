94d967935e4eb5a93f35bda3c0dcecdec47d23cb
Zac Pullar-Strecker <zacmps@gmail.com>
2018-07-05 14:45:30 +1200

    Make appveyor config more like travis

--- winpty/build.rs
+++ winpty/build.rs
@@ -1,11 +1,12 @@
 use std::fs::copy;
+use std::path::Path;
 
 fn main() {
     // The working directory for `cargo test` is in the deps folder, not the debug/release root
-    #[cfg(debug_assertions)]
-    copy("../assets/windows/x86_64/winpty-agent.exe", "target/debug/deps/winpty-agent.exe").unwrap();
-    #[cfg(not(debug_assertions))]
-    copy("../assets/windows/x86_64/winpty-agent.exe", "target/release/deps/winpty-agent.exe").unwrap();
-
-    println!("cargo:rerun-if-changed=../assets/windows/x86_64/winpty-agent.exe")
+    if cfg!(test) && Path::new("target").exists() {
+        #[cfg(debug_assertions)]
+        copy("../assets/windows/x86_64/winpty-agent.exe", "target/debug/deps/winpty-agent.exe").unwrap();
+        #[cfg(not(debug_assertions))]
+        copy("../assets/windows/x86_64/winpty-agent.exe", "target/release/deps/winpty-agent.exe").unwrap();
+    }
 }
