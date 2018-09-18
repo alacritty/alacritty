c0c5d3d7f1151a51aff53e05ee36b4ff0efe094a
Zac Pullar-Strecker <zacmps@gmail.com>
2018-09-18 18:10:58 +1200

    Remove vendored winpty-agent and fetch from nuget instead.
    Also add the scrollback section to alacritty_windows.yml

--- build.rs
+++ build.rs
@@ -13,12 +13,25 @@
 // limitations under the License.
 #[cfg(windows)]
 extern crate embed_resource;
+#[cfg(windows)]
+extern crate tempdir;
+#[cfg(windows)]
+extern crate reqwest;
+#[cfg(windows)]
+extern crate zip;
+
+#[cfg(windows)]
+use tempdir::TempDir;
+
 extern crate gl_generator;
 
 use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 use std::env;
-use std::fs::{copy, File};
+use std::fs::{File, OpenOptions};
 use std::path::Path;
+use std::io;
+
+const WINPTY_PACKAGE_URL: &str = "https://www.nuget.org/api/v2/package/winpty.NET/0.4.2";
 
 fn main() {
     let dest = env::var("OUT_DIR").unwrap();
@@ -34,10 +47,39 @@ fn main() {
         .unwrap();
 
     #[cfg(windows)]
-    embed_resource::compile("assets/windows/windows.rc");
+    {
+        embed_resource::compile("assets/windows/windows.rc");
 
-    #[cfg(debug_assertions)]
-    copy("assets/windows/x86_64/winpty-agent.exe", "target/debug/winpty-agent.exe").unwrap();
-    #[cfg(not(debug_assertions))]
-    copy("assets/windows/x86_64/winpty-agent.exe", "target/release/winpty-agent.exe").unwrap();
+        // Path is relative to target/{profile}/build/alacritty-HASH/out
+        let file = Path::new(&env::var("OUT_DIR").unwrap()).join("../../../winpty-agent.exe");
+        if !file.exists() {
+            aquire_winpty_agent(&file);
+        }
+    }
+}
+
+#[cfg(windows)]
+fn aquire_winpty_agent(out_path: &Path) {
+    let tmp_dir = TempDir::new("alacritty_build").unwrap();
+
+    let mut response = reqwest::get(WINPTY_PACKAGE_URL).unwrap();
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(tmp_dir.path().join("winpty_package.zip")).unwrap();
+
+    io::copy(&mut response, &mut file).unwrap();
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    let target = match env::var("TARGET").unwrap().split("-").next().unwrap() {
+        "x86_64" => "x86",
+        "i386" => "x64",
+        _ => panic!("architecture has no winpty binary")
+    };
+
+    let mut winpty_agent = archive.by_name(&format!("content/winpty/{}/winpty-agent.exe", target)).unwrap();
+
+    io::copy(&mut winpty_agent, &mut File::create(out_path).unwrap()).unwrap();
 }
