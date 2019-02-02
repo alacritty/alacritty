7fa4079e30a6abb4094ff258816a858878a41abb
M Stoeckl <code@mstoeckl.com>
2019-02-02 00:03:38 -0500

    Remove unused coordinate from rect shader

--- res/rect.v.glsl
+++ res/rect.v.glsl
@@ -12,14 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #version 330 core
-layout (location = 0) in vec3 aPos;
-
-out vec4 color;
-
-uniform vec4 col;
+layout (location = 0) in vec2 aPos;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    color = col;
+    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
 }
