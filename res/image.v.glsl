847eb88719ef540513abf38478f38efec2241952
tamamu <tamamu.1r1s@gmail.com>
2019-02-03 15:05:03 +0900

    Add image shader

---
+++ res/image.v.glsl
@@ -0,0 +1,27 @@
+// Copyright 2016 Joe Wilm, The Alacritty Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
+out vec2 TexCoord;
+out vec4 color;
+
+uniform vec4 col;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    color = col;
+    TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+}
