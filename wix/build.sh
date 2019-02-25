a3392a80f57d02b7980047a435a2f51b95036cbd
Erik Berkun-Drevnig <erik.b22@gmail.com>
2019-02-25 16:12:06 -0500

    Add shortcuts, fix installer issues

--- wix/build.sh
+++ wix/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-"WiX.3.11.1/tools/candle.exe" -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out "alacritty.wixobj" "alacritty.wxs"
+"WiX.3.11.1/tools/candle.exe" -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.wixobj" "wix/alacritty.wxs"
 
-"WiX.3.11.1/tools/light.exe" -nologo -ext WixUIExtension -ext WixUtilExtension -out "alacritty.msi" -sice:ICE57 "alacritty.wixobj"
+"WiX.3.11.1/tools/light.exe" -nologo -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.msi" -sice:ICE61 -sice:ICE91 "wix/alacritty.wixobj"
