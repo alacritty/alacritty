ed4789d63f88b9dd6dc7810b70fb0e4b6d423d42
Erik Berkun-Drevnig <erik.b22@gmail.com>
2019-02-26 17:03:58 -0500

    Change context menu text, remove redundant copying of config file

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -62,8 +62,8 @@ elif [ "$TRAVIS_OS_NAME" == "windows" ]; then
         "./target/release/winpty-agent.exe"
 
     # Create msi installer
-    "./WiX.3.11.1/tools/candle.exe" -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.wixobj" "wix/alacritty.wxs"
-    "./WiX.3.11.1/tools/light.exe" -nologo -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.msi" -sice:ICE61 -sice:ICE91 "wix/alacritty.wixobj"
+    ./WiX.*/tools/candle.exe -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.wixobj" "wix/alacritty.wxs"
+    ./WiX.*/tools/light.exe -nologo -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.msi" -sice:ICE61 -sice:ICE91 "wix/alacritty.wixobj"
     mv "./wix/alacritty.msi" "./target/deploy/${name}-windows.msi"
 fi
 
--- wix/build.sh
+++ wix/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-"WiX.3.11.1/tools/candle.exe" -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.wixobj" "wix/alacritty.wxs"
+WiX.*/tools/candle.exe -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.wixobj" "wix/alacritty.wxs"
 
-"WiX.3.11.1/tools/light.exe" -nologo -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.msi" -sice:ICE61 -sice:ICE91 "wix/alacritty.wixobj"
+WiX.*/tools/light.exe -nologo -ext WixUIExtension -ext WixUtilExtension -out "wix/alacritty.msi" -sice:ICE61 -sice:ICE91 "wix/alacritty.wixobj"
