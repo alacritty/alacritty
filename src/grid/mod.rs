3c4bb7c11518e1ced9feeb156436d55fe1d4a66b
Christian Duerr <chrisduerr@users.noreply.github.com>
2019-03-02 18:32:48 +0000

    Fix scrollback live reloading
    This fixes two issues with live reloading the `scrolling.history`
    property.
    
    It is now possible to increase the scrollback history without restarting
    Alacritty.
    
    When decreasing the scrollback history while scrolled beyond the new
    history limit, Alacritty will reset the viewport position to the new
    limit instead of crashing.
    
    This fixes #2147.

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -163,7 +163,9 @@ impl<T: Copy + Clone> Grid<T> {
     pub fn update_history(&mut self, history_size: usize, template: &T)
     {
         self.raw.update_history(history_size, Row::new(self.cols, &template));
+        self.max_scroll_limit = history_size;
         self.scroll_limit = min(self.scroll_limit, history_size);
+        self.display_offset = min(self.display_offset, self.scroll_limit);
     }
 
     pub fn scroll_display(&mut self, scroll: Scroll) {
