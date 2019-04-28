e6ea0ba36c0bb48d1d1a3e5e42daf6d3e7d8d120
Christian Duerr <contact@christianduerr.com>
2019-04-28 14:18:47 +0200

    Fix deployment

--- ci/script.sh
+++ ci/script.sh
@@ -14,6 +14,8 @@ fi
 
 # Run test in release mode if a tag is present, to produce an optimized binary
 if [ -n "$TRAVIS_TAG" ]; then
+    # Build separately so we generate an 'alacritty' binary without -HASH appended
+    cargo build --release
     cargo test --release
 else
     cargo test
