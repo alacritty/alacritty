34c54e7e0eb7c28406df112d118b82280be2adee
Nathan Lilienthal <nathan@nixpulvis.com>
2019-05-03 19:48:25 -0400

    Add git hash to version string, and fix crate name (#2397)
    This moves `cli` out of `alacritty_terminal` and into `alacritty` where
    it belongs, along with the `clap` dependency.

---
+++ alacritty/build.rs
@@ -0,0 +1,19 @@
+// Copyright 2019 Joe Wilm, The Alacritty Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+fn main() {
+    let hash = rustc_tools_util::get_commit_hash()
+        .expect("couldn't get commit hash");
+    println!("cargo:rustc-env=GIT_HASH={}", hash);
+}
