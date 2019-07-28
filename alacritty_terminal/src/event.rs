alacritty/alacritty

commit 84aca672964e29b5b4503b7da7bc34fc395f08ab

Change mouse cursor when hovering over the message bar

alacritty_terminal/src/input.rs
@@ -412,11 +412,19 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
 
         let cell_changed =
             prev_line != self.ctx.mouse().line || prev_col != self.ctx.mouse().column;
-        let mouse_moved = cell_changed || prev_side != cell_side;
+
+        // If the mouse hasn't changed cells, do nothing
+        if cell_changed || prev_side != cell_side {
+            return;
+        }
 
         // Only report motions when cell changed and mouse is not over the message bar
-        if self.message_at_point(Some(point)).is_some() || !mouse_moved {
+        if let Some(message) = self.message_at_point(Some(point)) {
+            self.update_message_cursor(point, message);
+
             return;
+        } else {
+            self.ctx.terminal_mut().reset_mouse_cursor();
         }
 
         // Don't launch URLs if mouse has moved
@@ -910,6 +918,15 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
         has_binding
     }
 
+    /// Set the cursor depending on where the mouse is on the message bar
+    fn update_message_cursor(&mut self, point: Point, message: Message) {
+        if self.message_close_at_point(point, message) {
+            self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Hand);
+        } else {
+            self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Default);
+        }
+    }
+
     /// Return the message bar's message if there is some at the specified point
     fn message_at_point(&mut self, point: Option<Point>) -> Option<Message> {
         if let (Some(point), Some(message)) =
@@ -924,16 +941,25 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
         None
     }
 
+    /// Whether the point is over the message bar's close button
+    fn message_close_at_point(&self, point: Point, message: Message) -> bool {
+        let size = self.ctx.size_info();
+        point.col + message_bar::CLOSE_BUTTON_TEXT.len() >= size.cols()
+            && point.line == size.lines() - message.text(&size).len()
+    }
+
     /// Handle clicks on the message bar.
     fn on_message_bar_click(&mut self, button_state: ElementState, point: Point, message: Message) {
         match button_state {
             ElementState::Released => self.copy_selection(),
             ElementState::Pressed => {
-                let size = self.ctx.size_info();
-                if point.col + message_bar::CLOSE_BUTTON_TEXT.len() >= size.cols()
-                    && point.line == size.lines() - message.text(&size).len()
-                {
+                if self.message_close_at_point(point, message) {
                     self.ctx.terminal_mut().message_buffer_mut().pop();
+                    if let Some(message) = self.message_at_point(Some(point)) {
+                        self.update_message_cursor(point, message);
+                    } else {
+                        self.ctx.terminal_mut().reset_mouse_cursor();
+                    }
                 }
 
                 self.ctx.clear_selection();

alacritty_terminal/src/term/mod.rs
@@ -1347,7 +1347,7 @@ impl Term {
     }
 
     #[inline]
-    pub fn reset_url_highlight(&mut self) {
+    pub fn reset_mouse_cursor(&mut self) {
         let mouse_mode =
             TermMode::MOUSE_MOTION | TermMode::MOUSE_DRAG | TermMode::MOUSE_REPORT_CLICK;
         let mouse_cursor = if self.mode().intersects(mouse_mode) {
@@ -1356,6 +1356,11 @@ impl Term {
             MouseCursor::Text
         };
         self.set_mouse_cursor(mouse_cursor);
+    }
+
+    #[inline]
+    pub fn reset_url_highlight(&mut self) {
+        self.reset_mouse_cursor();
 
         self.grid.url_highlight = None;
         self.dirty = true;
@@ -1404,6 +1409,7 @@ impl ansi::Handler for Term {
     #[inline]
     fn set_mouse_cursor(&mut self, cursor: MouseCursor) {
         self.next_mouse_cursor = Some(cursor);
+        self.dirty = true;
     }
 
     /// A character to be displayed

alacritty_terminal/src/window.rs
@@ -61,6 +61,9 @@ pub struct Window {
     windowed_context: glutin::WindowedContext<PossiblyCurrent>,
     mouse_visible: bool,
 
+    /// Keep track of the current mouse cursor to avoid unnecessarily changing it
+    current_mouse_cursor: MouseCursor,
+
     /// Whether or not the window is the focused window.
     pub is_focused: bool,
 }
@@ -164,8 +167,13 @@ impl Window {
         // Set OpenGL symbol loader. This call MUST be after window.make_current on windows.
         gl::load_with(|symbol| windowed_context.get_proc_address(symbol) as *const _);
 
-        let window =
-            Window { event_loop, windowed_context, mouse_visible: true, is_focused: false };
+        let window = Window {
+            event_loop,
+            current_mouse_cursor: MouseCursor::Default,
+            windowed_context,
+            mouse_visible: true,
+            is_focused: false,
+        };
 
         window.run_os_extensions();
 
@@ -239,8 +247,11 @@ impl Window {
     }
 
     #[inline]
-    pub fn set_mouse_cursor(&self, cursor: MouseCursor) {
-        self.window().set_cursor(cursor);
+    pub fn set_mouse_cursor(&mut self, cursor: MouseCursor) {
+        if cursor != self.current_mouse_cursor {
+            self.current_mouse_cursor = cursor;
+            self.window().set_cursor(cursor);
+        }
     }
 
     /// Set mouse cursor visible

commit ca273591a121581bbf4bb544efbe75959c7ba654

Fix cursor flickering on url hover.
This commit fixes the regression introduced in 84aca67 and also fixes url
highlight bounds computation when url ends on a last column.

Fixes #2665.

alacritty_terminal/src/event.rs
@@ -410,6 +410,7 @@ impl<N: Notify> Processor<N> {
                             processor.ctx.terminal.next_is_urgent = Some(false);
                         } else {
                             processor.ctx.terminal.reset_url_highlight();
+                            processor.ctx.terminal.reset_mouse_cursor();
                             processor.ctx.terminal.dirty = true;
                             *hide_mouse = false;
                         }

alacritty_terminal/src/input.rs
@@ -383,7 +383,30 @@ impl From<&'static str> for Action {
     }
 }
 
+enum MousePosition {
+    Url(Url),
+    MessageBar,
+    MessageBarButton,
+    Terminal,
+}
+
 impl<'a, A: ActionContext + 'a> Processor<'a, A> {
+    fn mouse_position(&mut self, point: Point) -> MousePosition {
+        if let Some(message) = self.message_at_point(Some(point)) {
+            if self.message_close_at_point(point, message) {
+                MousePosition::MessageBarButton
+            } else {
+                MousePosition::MessageBar
+            }
+        // Check for url should be after check for message bar, since we're not looking into
+        // message bar content.
+        } else if let Some(url) = self.ctx.terminal().url_search(point.into()) {
+            MousePosition::Url(url)
+        } else {
+            MousePosition::Terminal
+        }
+    }
+
     #[inline]
     pub fn mouse_moved(&mut self, x: usize, y: usize, modifiers: ModifiersState) {
         self.ctx.mouse_mut().x = x;
@@ -408,21 +431,38 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
             return;
         }
 
-        // Only report motions when cell changed and mouse is not over the message bar
-        if let Some(message) = self.message_at_point(Some(point)) {
-            self.update_message_cursor(point, message);
-
-            return;
-        } else {
-            self.ctx.terminal_mut().reset_mouse_cursor();
-        }
-
         // Don't launch URLs if mouse has moved
         self.ctx.mouse_mut().block_url_launcher = true;
 
-        // Underline URLs and change cursor on hover
-        if cell_changed {
-            self.update_url_highlight(point, modifiers);
+        match self.mouse_position(point) {
+            MousePosition::Url(url) => {
+                let mouse_mode =
+                    TermMode::MOUSE_MOTION | TermMode::MOUSE_DRAG | TermMode::MOUSE_REPORT_CLICK;
+
+                if self.mouse_config.url.mods().relaxed_eq(modifiers)
+                    && (!self.ctx.terminal().mode().intersects(mouse_mode) || modifiers.shift)
+                    && self.mouse_config.url.launcher.is_some()
+                {
+                    let url_bounds = self.url_bounds_at_point(url, point);
+                    self.ctx.terminal_mut().set_url_highlight(url_bounds);
+                    self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Hand);
+                    self.ctx.terminal_mut().dirty = true;
+                }
+            },
+            MousePosition::MessageBar => {
+                self.ctx.terminal_mut().reset_url_highlight();
+                self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Default);
+                return;
+            },
+            MousePosition::MessageBarButton => {
+                self.ctx.terminal_mut().reset_url_highlight();
+                self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Hand);
+                return;
+            },
+            MousePosition::Terminal => {
+                self.ctx.terminal_mut().reset_url_highlight();
+                self.ctx.terminal_mut().reset_mouse_cursor();
+            },
         }
 
         if self.ctx.mouse().left_button_state == ElementState::Pressed
@@ -445,50 +485,45 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
         }
     }
 
-    /// Underline URLs and change the mouse cursor when URL hover state changes.
-    fn update_url_highlight(&mut self, point: Point, modifiers: ModifiersState) {
-        let mouse_mode =
-            TermMode::MOUSE_MOTION | TermMode::MOUSE_DRAG | TermMode::MOUSE_REPORT_CLICK;
+    fn url_bounds_at_point(&self, url: Url, point: Point) -> RangeInclusive<Linear> {
+        let Url { origin, text } = url;
+        let cols = self.ctx.size_info().cols().0;
 
-        // Only show URLs as launchable when all required modifiers are pressed
-        let url = if self.mouse_config.url.mods().relaxed_eq(modifiers)
-            && (!self.ctx.terminal().mode().intersects(mouse_mode) || modifiers.shift)
-            && self.mouse_config.url.launcher.is_some()
-        {
-            self.ctx.terminal().url_search(point.into())
+        // Calculate the URL's start position
+        let lines_before = (origin + cols - point.col.0 - 1) / cols;
+        let (start_col, start_line) = if lines_before > point.line.0 {
+            (0, 0)
         } else {
-            None
+            let start_col = (cols + point.col.0 - origin % cols) % cols;
+            let start_line = point.line.0 - lines_before;
+            (start_col, start_line)
         };
 
-        if let Some(Url { origin, text }) = url {
-            let cols = self.ctx.size_info().cols().0;
+        let start = Point::new(start_line, Column(start_col));
 
-            // Calculate the URL's start position
-            let lines_before = (origin + cols - point.col.0 - 1) / cols;
-            let (start_col, start_line) = if lines_before > point.line.0 {
-                (0, 0)
-            } else {
-                let start_col = (cols + point.col.0 - origin % cols) % cols;
-                let start_line = point.line.0 - lines_before;
-                (start_col, start_line)
-            };
-            let start = Point::new(start_line, Column(start_col));
+        // Calculate the URL's highlight end position
+        let len = text.width();
+        let url_end_col_denormilized = point.col.0 + len - origin;
 
-            // Calculate the URL's end position
-            let len = text.width();
-            let end_col = (point.col.0 + len - origin) % cols - 1;
-            let end_line = point.line.0 + (point.col.0 + len - origin) / cols;
-            let end = Point::new(end_line, Column(end_col));
-
-            let start = Linear::from_point(Column(cols), start);
-            let end = Linear::from_point(Column(cols), end);
-
-            self.ctx.terminal_mut().set_url_highlight(RangeInclusive::new(start, end));
-            self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Hand);
-            self.ctx.terminal_mut().dirty = true;
+        // This means that url ends at the last cell of the line
+        let end_col = if url_end_col_denormilized % cols == 0 {
+            cols - 1
         } else {
-            self.ctx.terminal_mut().reset_url_highlight();
-        }
+            url_end_col_denormilized % cols - 1
+        };
+
+        let end_line = if end_col == cols - 1 {
+            point.line.0 + (url_end_col_denormilized) / cols - 1
+        } else {
+            point.line.0 + (url_end_col_denormilized) / cols
+        };
+
+        let end = Point::new(end_line, Column(end_col));
+
+        let start = Linear::from_point(Column(cols), start);
+        let end = Linear::from_point(Column(cols), end);
+
+        RangeInclusive::new(start, end)
     }
 
     fn get_mouse_side(&self) -> Side {
@@ -908,15 +943,6 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
         has_binding
     }
 
-    /// Set the cursor depending on where the mouse is on the message bar
-    fn update_message_cursor(&mut self, point: Point, message: Message) {
-        if self.message_close_at_point(point, message) {
-            self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Hand);
-        } else {
-            self.ctx.terminal_mut().set_mouse_cursor(MouseCursor::Default);
-        }
-    }
-
     /// Return the message bar's message if there is some at the specified point
     fn message_at_point(&mut self, point: Option<Point>) -> Option<Message> {
         if let (Some(point), Some(message)) =
@@ -945,11 +971,7 @@ impl<'a, A: ActionContext + 'a> Processor<'a, A> {
             ElementState::Pressed => {
                 if self.message_close_at_point(point, message) {
                     self.ctx.terminal_mut().message_buffer_mut().pop();
-                    if let Some(message) = self.message_at_point(Some(point)) {
-                        self.update_message_cursor(point, message);
-                    } else {
-                        self.ctx.terminal_mut().reset_mouse_cursor();
-                    }
+                    self.ctx.terminal_mut().reset_mouse_cursor();
                 }
 
                 self.ctx.clear_selection();

alacritty_terminal/src/term/mod.rs
@@ -896,6 +896,7 @@ impl Term {
     pub fn scroll_display(&mut self, scroll: Scroll) {
         self.grid.scroll_display(scroll);
         self.reset_url_highlight();
+        self.reset_mouse_cursor();
         self.dirty = true;
     }
 
@@ -1360,8 +1361,6 @@ impl Term {
 
     #[inline]
     pub fn reset_url_highlight(&mut self) {
-        self.reset_mouse_cursor();
-
         self.grid.url_highlight = None;
         self.dirty = true;
     }