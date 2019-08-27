14b1b6cf3a925fba2abf081e7068feb30f73f00d
thunderseethe <thunderseethe.dev@gmail.com>
2019-08-27 11:52:52 -0700

    Fix test code for macosx

--- font/src/darwin/mod.rs
+++ font/src/darwin/mod.rs
@@ -589,7 +589,7 @@ mod tests {
         for font in fonts {
             // Get a glyph
             for c in &['a', 'b', 'c', 'd'] {
-                let glyph = font.get_glyph(*c, 72., false).unwrap();
+                let glyph = font.get_glyph((*c).into(), 72., false).unwrap();
 
                 // Debug the glyph.. sigh
                 for row in 0..glyph.height {
