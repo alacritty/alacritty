715263ac3a6c298a88a73db418313555a2af2091
thunderseethe <thunderseethe.dev@gmail.com>
2019-09-10 15:27:30 -0700

    Refactor text_run for clarity of intent and clean up dead code.

--- alacritty_terminal/src/text_run.rs
+++ alacritty_terminal/src/text_run.rs
@@ -8,44 +8,22 @@ use crate::term::{
 
 #[derive(Debug)]
 struct RunStart {
-    pub line: Line,
-    pub column: Column,
-    pub fg: Rgb,
-    pub bg: Rgb,
-    pub bg_alpha: f32,
-    pub flags: Flags,
+    line: Line,
+    column: Column,
+    fg: Rgb,
+    bg: Rgb,
+    bg_alpha: f32,
+    flags: Flags,
 }
-
-// Use a macro instead of a function to make use of partial move semantics that don't cross
-// function boundaries
-// Convert a RenderableCell into a RunStart
-macro_rules! from_rc {
-    ($rc:ident) => {
-        RunStart {
-            line: $rc.line,
-            column: $rc.column,
-            fg: $rc.fg,
-            bg: $rc.bg,
-            bg_alpha: $rc.bg_alpha,
-            flags: $rc.flags,
-        }
-    };
-}
-
-/// Compare cells and check they are in the same text run
-fn is_adjacent_context(a: &RunStart, b: &RenderableCell) -> bool {
-    a.line == b.line
-        && a.fg == b.fg
-        && a.bg == b.bg
-        && (a.bg_alpha - b.bg_alpha).abs() < std::f32::EPSILON
-        && a.flags == b.flags
-}
-
-type LatestCol = (Column, bool);
-/// Checks two columns are adjacent
-fn is_adjacent_col((a, is_wide): LatestCol, b: Column) -> bool {
-    let width = if is_wide { 2usize } else { 1usize };
-    a + width == b || b + width == a
+impl RunStart {
+    /// Compare cell and check if it belongs to the same run.
+    fn is_adjacent_to_cell(&self, rc: &RenderableCell) -> bool {
+        self.line == rc.line
+            && self.fg == rc.fg
+            && self.bg == rc.bg
+            && (self.bg_alpha - rc.bg_alpha).abs() < std::f32::EPSILON
+            && self.flags == rc.flags
+    }
 }
 
 #[derive(Debug)]
@@ -60,7 +38,7 @@ pub enum TextRunContent {
 /// (e.g. selection hightlight) which is desired behavior.
 #[derive(Debug)]
 pub struct TextRun {
-    /// By definition a run is on one line.
+    /// A run never spans multiple lines.
     pub line: Line,
     /// Span of columns the text run covers.
     pub span: (Column, Column),
@@ -70,14 +48,13 @@ pub struct TextRun {
     pub fg: Rgb,
     /// Background color of text run content.
     pub bg: Rgb,
-    /// Opacity of background color of text run content.
+    /// Background color opacity of the text run
     pub bg_alpha: f32,
-    /// Attributes of this text run (e.g. HIDDEN, STRIKETHROUGH, etc.)
+    /// Attributes of this text run.
     pub flags: Flags,
 }
 
 impl TextRun {
-    // These two constructors are used by TextRunIter and are not widely applicable
     fn from_iter_state(
         start: RunStart,
         (latest, is_wide): LatestCol,
@@ -120,35 +97,9 @@ impl TextRun {
         }
     }
 
-    /// Number of columns this TextRun spans
-    pub fn len(&self) -> usize {
-        let (start, end) = self.span;
-        end.0 - start.0
-    }
-
-    /// True if TextRun contains characters, false if it contains no characters.
-    pub fn is_empty(&self) -> bool {
-        match &self.content {
-            TextRunContent::Cursor(_) => false,
-            TextRunContent::CharRun(ref string, ref zero_widths) => {
-                string.is_empty() && zero_widths.is_empty()
-            },
-        }
-    }
-
-    /// First column of the run
-    pub fn start_col(&self) -> Column {
-        self.span.0
-    }
-
     /// First cell in the TextRun
     pub fn start_cell(&self) -> RenderableCell {
         self.cell_at(self.span.0)
-    }
-
-    /// End cell in the TextRun
-    pub fn end_cell(&self) -> RenderableCell {
-        self.cell_at(self.span.1)
     }
 
     /// First point covered by this TextRun
@@ -161,31 +112,23 @@ impl TextRun {
         Point { line: self.line, col: self.span.1 }
     }
 
-    /// Returns iterator over range of columns [run.0, run.1]
-    pub fn cols(&self) -> impl Iterator<Item = Column> {
-        let (start, end) = self.span;
-        let step = if self.flags.contains(Flags::WIDE_CHAR) {
-            // If our run contains ide chars treat each cell like it's 2 cells wide
-            2
-        } else {
-            1
-        };
-        // unpacking is neccessary while Step trait is nightly
-        // hopefully this compiles away.
-        (start.0..=end.0).step_by(step).map(Column)
-    }
-
     /// Iterates over each RenderableCell in column range [run.0, run.1]
     pub fn cells<'a>(&'a self) -> impl Iterator<Item = RenderableCell> + 'a {
-        self.cols().map(move |col| self.cell_at(col))
+        let step = if self.flags.contains(Flags::WIDE_CHAR) { 2 } else { 1 };
+        let (Column(start), Column(end)) = self.span;
+        // TODO: impl Step for Column (once Step is stable) to avoid unwrapping then rewrapping.
+        (start..=end).step_by(step).map(move |col| self.cell_at(Column(col)))
     }
 }
+
+type IsWide = bool;
+type LatestCol = (Column, IsWide);
 
 /// Wraps an Iterator<Item=RenderableCell> and produces TextRuns to represent batches of cells
 pub struct TextRunIter<I> {
     iter: I,
     run_start: Option<RunStart>,
-    latest: Option<LatestCol>,
+    latest_col: Option<LatestCol>,
     cursor: Option<CursorKey>,
     buffer_text: String,
     buffer_zero_width: Vec<[char; MAX_ZEROWIDTH_CHARS]>,
@@ -205,7 +148,7 @@ where
             // Logic for WIDE_CHAR is handled internally by TextRun
             // So we no longer need WIDE_CHAR_SPACER at this point.
             iter: iter.filter(is_not_wide_char_spacer),
-            latest: None,
+            latest_col: None,
             run_start: None,
             cursor: None,
             buffer_text: String::new(),
@@ -214,13 +157,23 @@ where
     }
 }
 impl<I> TextRunIter<I> {
-    /// Check if current run ends with incoming RenderableCell
+    /// Check if current run ends at incoming RenderableCell
+    /// Run will not include incoming RenderableCell if it ends
     fn is_end_of_run(&self, rc: &RenderableCell) -> bool {
-        let is_cell_adjacent =
-            self.run_start.as_ref().map(|cell| !is_adjacent_context(cell, &rc)).unwrap_or(false);
-        let is_col_adjacent =
-            self.latest.as_ref().map(|col| !is_adjacent_col(*col, rc.column)).unwrap_or(false);
-        is_cell_adjacent || is_col_adjacent
+        let is_cell_not_adjacent = self
+            .run_start
+            .as_ref()
+            .map(|run_start| !run_start.is_adjacent_to_cell(rc))
+            .unwrap_or(false);
+        let is_col_not_adjacent = self
+            .latest_col
+            .as_ref()
+            .map(|&(col, is_wide)| {
+                let width = if is_wide { 2usize } else { 1usize };
+                col + width != rc.column && rc.column + width != col
+            })
+            .unwrap_or(false);
+        is_cell_not_adjacent || is_col_not_adjacent
     }
 
     /// Add content of cell to pending TextRun buffer
@@ -244,11 +197,19 @@ impl<I> TextRunIter<I> {
         (self.buffer_text.drain(..).collect(), self.buffer_zero_width.drain(..).collect())
     }
 
-    /// Handles bookkeeping needed when starting a new run
+    /// Start a new run by setting latest_col, run_start, and buffering content of rc
+    /// Returns the previous runs run_start and latest_col data if available.
     fn start_run(&mut self, rc: RenderableCell) -> (Option<RunStart>, Option<LatestCol>) {
         self.buffer_content(rc.inner);
-        let latest = self.latest.replace((rc.column, rc.flags.contains(Flags::WIDE_CHAR)));
-        let start = self.run_start.replace(from_rc!(rc));
+        let latest = self.latest_col.replace((rc.column, rc.flags.contains(Flags::WIDE_CHAR)));
+        let start = self.run_start.replace(RunStart {
+            line: rc.line,
+            column: rc.column,
+            fg: rc.fg,
+            bg: rc.bg,
+            bg_alpha: rc.bg_alpha,
+            flags: rc.flags,
+        });
         (start, latest)
     }
 
@@ -267,10 +228,10 @@ impl<I> TextRunIter<I> {
     /// completion.
     fn produce_char_run(&mut self, rc: RenderableCell) -> Option<TextRun> {
         let prev_buffer = self.drain_buffer();
-        let (start_opt, latest_opt) = self.start_run(rc);
+        let (start_opt, latest_col_opt) = self.start_run(rc);
         let start = start_opt?;
-        let latest = latest_opt?;
-        Some(TextRun::from_iter_state(start, latest, prev_buffer))
+        let latest_col = latest_col_opt?;
+        Some(TextRun::from_iter_state(start, latest_col, prev_buffer))
     }
 }
 
@@ -283,10 +244,17 @@ where
     fn next(&mut self) -> Option<Self::Item> {
         let mut output = None;
         while let Some(rc) = self.iter.next() {
-            if self.latest.is_none() || self.run_start.is_none() {
+            if self.latest_col.is_none() || self.run_start.is_none() {
                 // Initial state, this is should only be hit on the first next() call of
                 // iterator
-                self.run_start = Some(from_rc!(rc));
+                self.run_start = Some(RunStart {
+                    line: rc.line,
+                    column: rc.column,
+                    fg: rc.fg,
+                    bg: rc.bg,
+                    bg_alpha: rc.bg_alpha,
+                    flags: rc.flags,
+                })
             } else if self.cursor.is_some() {
                 // Last iteration of the loop found a cursor
                 // Return a run for the cursor and start a new run
@@ -299,18 +267,17 @@ where
                 break;
             }
             // Build up buffer and track the latest column we've seen
-            self.latest = Some((rc.column, rc.flags.contains(Flags::WIDE_CHAR)));
+            self.latest_col = Some((rc.column, rc.flags.contains(Flags::WIDE_CHAR)));
             self.buffer_content(rc.inner);
         }
-        // If we generated output we're done.
-        // Otherwise check for any remaining buffered content and return it as a text run.
+        // Check for any remaining buffered content and return it as a text run.
         // This is a destructive operation, it will return None after it excutes once.
         output.or_else(|| {
             if !self.buffer_text.is_empty() || !self.buffer_zero_width.is_empty() {
                 let start = self.run_start.take()?;
-                let latest = self.latest.take()?;
+                let latest_col = self.latest_col.take()?;
                 // Save leftover buffer and empty it
-                Some(TextRun::from_iter_state(start, latest, self.drain_buffer()))
+                Some(TextRun::from_iter_state(start, latest_col, self.drain_buffer()))
             } else if let Some(cursor) = self.cursor {
                 let start = self.run_start.take()?;
                 Some(TextRun::from_cursor_rc(start, cursor))
