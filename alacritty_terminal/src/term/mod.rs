alacritty/alacritty

commit 9a0555bbba30c264f617ec9260ca00e0eab70870

Fix cell opacity when color matches terminal bg
Commit e964af8 introduced a regression, where if cell's bg color was
equal to NamedColor::Background rgb color it was rendered with transparent
background. However the correct behavior is to render bg transparent
only when bg color is actually a NamedColor::Background.

Fixes #2814.

alacritty_terminal/src/term/mod.rs
@@ -273,11 +273,13 @@ impl RenderableCell {
         // Lookup RGB values
         let mut fg_rgb = Self::compute_fg_rgb(config, colors, cell.fg, cell.flags);
         let mut bg_rgb = Self::compute_bg_rgb(colors, cell.bg);
+        let mut bg_alpha = Self::compute_bg_alpha(cell.bg);
 
         let selection_background = config.colors.selection.background;
         if let (true, Some(col)) = (selected, selection_background) {
             // Override selection background with config colors
             bg_rgb = col;
+            bg_alpha = 1.0;
         } else if selected ^ cell.inverse() {
             if fg_rgb == bg_rgb && !cell.flags.contains(Flags::HIDDEN) {
                 // Reveal inversed text when fg/bg is the same
@@ -300,7 +302,7 @@ impl RenderableCell {
             inner: RenderableCellContent::Chars(cell.chars()),
             fg: fg_rgb,
             bg: bg_rgb,
-            bg_alpha: Self::compute_bg_alpha(colors, bg_rgb),
+            bg_alpha,
             flags: cell.flags,
         }
     }
@@ -343,8 +345,8 @@ impl RenderableCell {
     }
 
     #[inline]
-    fn compute_bg_alpha(colors: &color::List, bg: Rgb) -> f32 {
-        if colors[NamedColor::Background] == bg {
+    fn compute_bg_alpha(bg: Color) -> f32 {
+        if bg == Color::Named(NamedColor::Background) {
             0.
         } else {
             1.

commit 3184a1cd770389e73e6873b57899caff83a205f0

Fix selection not inverting terminal background
Fixes a regression introduced in
9a0555bbba30c264f617ec9260ca00e0eab70870 where the terminal background
would not get inverted when selected.

alacritty_terminal/src/term/mod.rs
@@ -289,6 +289,8 @@ impl RenderableCell {
                 // Invert cell fg and bg colors
                 mem::swap(&mut fg_rgb, &mut bg_rgb);
             }
+
+            bg_alpha = 1.0;
         }
 
         // Override selection text with config colors