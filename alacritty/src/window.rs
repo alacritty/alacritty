3fa609df0a98f5788505b309da5c5ac1e2b93667
Kohki Miki <giginet.net@gmail.com>
2019-10-22 18:33:37 +0900

    Update alacritty/src/window.rs

--- alacritty/src/window.rs
+++ alacritty/src/window.rs
@@ -15,7 +15,7 @@ use std::convert::From;
 #[cfg(not(any(target_os = "macos", windows)))]
 use std::ffi::c_void;
 use std::fmt;
-#[cfg(target_os = "linux")]
+#[cfg(not(any(target_os = "macos", windows)))]
 use std::os::raw::c_ulong;
 
 use glutin::dpi::{LogicalPosition, LogicalSize, PhysicalPosition, PhysicalSize};
