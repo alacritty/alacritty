9e1704d94e8c9d1d9d31544f903eb049f6d3edae
Nathan Lilienthal <nathan@nixpulvis.com>
2019-11-11 18:40:06 -0500

    Reset the cleared screen to the template.

--- alacritty_terminal/src/term/mod.rs
+++ alacritty_terminal/src/term/mod.rs
@@ -1793,11 +1793,10 @@ impl<T: EventListener> ansi::Handler for Term<T> {
                 }
             },
             ansi::ClearMode::All => {
-                if self.mode.contains(TermMode::ALT_SCREEN) {
-                    self.grid.region_mut(..).each(|c| c.reset(&template));
-                } else {
+                if !self.mode.contains(TermMode::ALT_SCREEN) {
                     self.grid.clear_viewport(&template);
                 }
+                self.grid.region_mut(..).each(|c| c.reset(&template));
             },
             ansi::ClearMode::Saved => self.grid.clear_history(),
         }
