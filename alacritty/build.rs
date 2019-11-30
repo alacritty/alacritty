5f98de46921a878bc13397859eadd7ed4f7c2b44
sterlingjensen <5555776+sterlingjensen@users.noreply.github.com>
2019-11-30 00:11:43 -0600

    Move icon embed step from winpty to Alacritty

--- alacritty/build.rs
+++ alacritty/build.rs
@@ -18,6 +18,9 @@ use std::env;
 use std::fs::File;
 use std::path::Path;
 
+#[cfg(windows)]
+use embed_resource;
+
 fn main() {
     let hash = rustc_tools_util::get_commit_hash().unwrap_or_default();
     println!("cargo:rustc-env=GIT_HASH={}", hash);
@@ -28,4 +31,7 @@ fn main() {
     Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, ["GL_ARB_blend_func_extended"])
         .write_bindings(GlobalGenerator, &mut file)
         .unwrap();
+
+    #[cfg(windows)]
+    embed_resource::compile("../extra/windows/windows.rc");
 }
