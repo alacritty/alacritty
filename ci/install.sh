640b1d7a910f39b46cf0271c192b45cd24a52498
Christian Duerr <contact@christianduerr.com>
2019-12-21 00:44:05 +0100

    Switch from rustup to travis rust version

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Switch from GNU to MSVC toolchain on Windows
-if [ "$TRAVIS_OS_NAME" == "windows" ]; then
-    rustup default "${TRAVIS_RUST_VERSION}-x86_64-pc-windows-msvc"
-fi
-
 # Add clippy for lint validation
 if [ "$CLIPPY" == "true" ]; then
     rustup component add clippy
