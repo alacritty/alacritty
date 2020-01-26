c54be3bd5a590fe023b1eab4195d2cc6bacd9440
Christian Duerr <contact@christianduerr.com>
2020-01-26 22:17:10 +0100

    Switch to flamegraph-rs script
    This cleans up the Alacritty scripts a bit by removing some of them
    which are not recommended to be used anymore and switching from the
    official FlameGraph tool to the more specialized Rust FlameGraph
    implementation.

--- scripts/create-flamegraph.sh
+++ scripts/create-flamegraph.sh
@@ -3,36 +3,29 @@
 # The full path to the script directory, regardless of pwd.
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# Current UNIX time.
-TIME=$(date +%s)
-
-# Make sure FlameGraph scripts are available.
-if [ ! -e $DIR/FlameGraph ]
-then
-    git clone https://github.com/BrendanGregg/FlameGraph \
-        $DIR/create-flamegraph/FlameGraph
-fi
-
-# Make sure a release build of Alacritty is available.
-if [ ! -e $DIR/../target/release/alacritty ]
-then
-    echo "Must build alacritty first: cargo build --release"
-    exit 1
-fi
-
 # Make sure perf is available.
 if [ ! -x "$(command -v perf)" ]
 then
-    echo "Cannot find perf, please make sure it's installed"
+    echo "Cannot find perf, please make sure it's installed."
     exit 1
 fi
 
-# Run perf, this will block while alacritty runs.
-perf record -g -F 99 $DIR/../target/release/alacritty
-perf script \
-    | $DIR/create-flamegraph/FlameGraph/stackcollapse-perf.pl \
-    | $DIR/create-flamegraph/FlameGraph/flamegraph.pl --width 1920 \
-    > flame-$TIME.svg
+# Install cargo-flamegraph
+installed_flamegraph=0
+if [ ! -x "$(command -v cargo-flamegraph)" ]; then
+    echo "cargo-flamegraph not installed; installing ..."
+    cargo install flamegraph
+    installed_flamegraph=1
+fi
 
-# Tell users where the file is.
-echo "Flame graph created at: file://$(pwd)/flame-$TIME.svg"
+# Create flamegraph
+cargo flamegraph --bin=alacritty -- -e true
+
+# Unintall cargo-flamegraph if it has been installed with this script
+if [ $installed_flamegraph == 1 ]; then
+    read -p "Would you like to uninstall cargo-flamegraph? [Y/n] " -n 1 -r
+    echo
+    if [[ "$REPLY" =~ ^[^Nn]*$ ]]; then
+        cargo uninstall flamegraph
+    fi
+fi
