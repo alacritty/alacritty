eb1a28ce581fbd42a5e0e1418d7c2c070cfe7545
Christian Duerr <contact@christianduerr.com>
2020-02-17 16:24:27 +0000

    Extract winpty crate
    The winpty crate and its winpty-sys depedency have been moved to
    https://github.com/alacritty/winpty.

--- winpty/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![deny(clippy::all, clippy::if_not_else, clippy::enum_glob_use, clippy::wrong_pub_self_convention)]
-
-#[cfg(windows)]
-pub mod windows;
-
-#[cfg(windows)]
-pub use crate::windows::*;
