46a4c80bc1c079a73d15500a57eb9edd0c4b0c1e
Kirill Chibisov <contact@kchibisov.com>
2020-03-20 05:27:12 +0300

    Update alacritty/src/window.rs
    Co-Authored-By: Cole Helbling <cole.e.helbling@outlook.com>

--- alacritty/src/window.rs
+++ alacritty/src/window.rs
@@ -35,7 +35,7 @@ use x11_dl::xlib::{Display as XDisplay, PropModeReplace, XErrorEvent, Xlib};
 
 #[cfg(not(any(target_os = "macos", windows)))]
 use alacritty_terminal::config::Colors;
-use alacritty_terminal::config::{Colors, Decorations, StartupMode, WindowConfig};
+use alacritty_terminal::config::{Decorations, StartupMode, WindowConfig};
 use alacritty_terminal::event::Event;
 #[cfg(not(windows))]
 use alacritty_terminal::term::{SizeInfo, Term};
