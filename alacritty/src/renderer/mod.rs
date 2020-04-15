82bc24b2d9aa1e5b666c840e242b0118268ad46c
Kirill Chibisov <contact@kchibisov.com>
2020-04-15 05:19:08 +0300

    Update alacritty/src/renderer/mod.rs
    Co-Authored-By: Christian Duerr <contact@christianduerr.com>

--- alacritty/src/renderer/mod.rs
+++ alacritty/src/renderer/mod.rs
@@ -342,6 +342,7 @@ impl GlyphCache {
         self.metrics
     }
 
+    /// Prefetch glyphs that are almost guaranteed to be loaded anyways.
     fn load_common_glyphs<L: LoadGlyph>(&mut self, loader: &mut L) {
         self.load_glyphs_for_font(self.font_key, loader);
         self.load_glyphs_for_font(self.bold_italic_key, loader);
