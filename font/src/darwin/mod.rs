alacritty/alacritty

commit 77f2d6e853f1ad54e6dc844a811b78daeb463e76

Fix emojis being blended with background
Fixes #1864.

font/src/darwin/mod.rs
@@ -488,8 +488,8 @@ impl Font {
             kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host,
         );
 
-        // Give the context an opaque, black background.
-        cg_context.set_rgb_fill_color(0.0, 0.0, 0.0, 1.0);
+        // Set background color for graphics context.
+        cg_context.set_rgb_fill_color(0.0, 0.0, 0.0, 0.0);
         let context_rect = CGRect::new(
             &CGPoint::new(0.0, 0.0),
             &CGSize::new(f64::from(rasterized_width), f64::from(rasterized_height)),

alacritty/src/renderer/mod.rs
@@ -418,6 +418,8 @@ struct InstanceData {
     bg_g: f32,
     bg_b: f32,
     bg_a: f32,
+    // Flag indicating that glyph uses multiple colors, like an Emoji.
+    multicolor: u8,
 }
 
 #[derive(Debug)]
@@ -465,17 +467,9 @@ impl Batch {
         Self { tex: 0, instances: Vec::with_capacity(BATCH_MAX) }
     }
 
-    pub fn add_item(&mut self, mut cell: RenderableCell, glyph: &Glyph) {
+    pub fn add_item(&mut self, cell: RenderableCell, glyph: &Glyph) {
         if self.is_empty() {
             self.tex = glyph.tex_id;
-        }
-
-        if glyph.colored {
-            // XXX Temporary workaround to prevent emojis being rendered with a wrong colors on, at
-            // least, dark backgrounds. For more info see #1864.
-            cell.fg.r = 255;
-            cell.fg.g = 255;
-            cell.fg.b = 255;
         }
 
         self.instances.push(InstanceData {
@@ -500,6 +494,7 @@ impl Batch {
             bg_g: f32::from(cell.bg.g),
             bg_b: f32::from(cell.bg.b),
             bg_a: cell.bg_alpha,
+            multicolor: glyph.colored as u8,
         });
     }
 
@@ -643,6 +638,17 @@ impl QuadRenderer {
             );
             gl::EnableVertexAttribArray(4);
             gl::VertexAttribDivisor(4, 1);
+            // Multicolor flag.
+            gl::VertexAttribPointer(
+                5,
+                1,
+                gl::BYTE,
+                gl::FALSE,
+                size_of::<InstanceData>() as i32,
+                (17 * size_of::<f32>()) as *const _,
+            );
+            gl::EnableVertexAttribArray(5);
+            gl::VertexAttribDivisor(5, 1);
 
             // Rectangle setup.
             gl::GenVertexArrays(1, &mut rect_vao);
@@ -1513,14 +1519,16 @@ impl Atlas {
             gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1);
             gl::GenTextures(1, &mut id);
             gl::BindTexture(gl::TEXTURE_2D, id);
+            // Use RGBA texture for both normal and emoji glyphs, since it has no performance
+            // impact.
             gl::TexImage2D(
                 gl::TEXTURE_2D,
                 0,
-                gl::RGB as i32,
+                gl::RGBA as i32,
                 size,
                 size,
                 0,
-                gl::RGB,
+                gl::RGBA,
                 gl::UNSIGNED_BYTE,
                 ptr::null(),
             );

font/src/lib.rs
@@ -173,7 +173,10 @@ pub struct RasterizedGlyph {
 
 #[derive(Clone, Debug)]
 pub enum BitmapBuffer {
+    /// RGB alphamask.
     RGB(Vec<u8>),
+
+    /// RGBA pixels with premultiplied alpha.
     RGBA(Vec<u8>),
 }
 

res/text.f.glsl
@@ -15,6 +15,7 @@
 in vec2 TexCoords;
 flat in vec3 fg;
 flat in vec4 bg;
+flat in int colored;
 uniform int backgroundPass;
 
 layout(location = 0, index = 0) out vec4 color;
@@ -31,8 +32,22 @@ void main()
         alphaMask = vec4(1.0);
         color = vec4(bg.rgb, 1.0);
     } else {
-        vec3 textColor = texture(mask, TexCoords).rgb;
-        alphaMask = vec4(textColor, textColor.r);
-        color = vec4(fg, 1.0);
+        if (colored != 0) {
+            // Color glyphs, like emojis.
+            vec4 glyphColor = texture(mask, TexCoords);
+            alphaMask = vec4(glyphColor.a);
+
+            // Revert alpha premultiplication.
+            if (glyphColor.a != 0) {
+                glyphColor.rgb = vec3(glyphColor.rgb / glyphColor.a);
+            }
+
+            color = vec4(glyphColor.rgb, 1.0);
+        } else {
+            // Regular text glyphs.
+            vec3 textColor = texture(mask, TexCoords).rgb;
+            alphaMask = vec4(textColor, textColor.r);
+            color = vec4(fg, 1.0);
+        }
     }
 }

res/text.v.glsl
@@ -12,24 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #version 330 core
-// Cell properties
+// Cell properties.
 layout (location = 0) in vec2 gridCoords;
 
-// glyph properties
+// Glyph properties.
 layout (location = 1) in vec4 glyph;
 
-// uv mapping
+// uv mapping.
 layout (location = 2) in vec4 uv;
 
-// text fg color
+// Text fg color.
 layout (location = 3) in vec3 textColor;
 
-// Background color
+// Background color.
 layout (location = 4) in vec4 backgroundColor;
+
+// Set to 1 if the glyph colors should be kept.
+layout (location = 5) in int coloredGlyph;
 
 out vec2 TexCoords;
 flat out vec3 fg;
 flat out vec4 bg;
+flat out int colored;
 
 // Terminal properties
 uniform vec2 cellDim;
@@ -71,4 +75,5 @@ void main()
 
     bg = vec4(backgroundColor.rgb / 255.0, backgroundColor.a);
     fg = textColor / vec3(255.0, 255.0, 255.0);
+    colored = coloredGlyph;
 }

commit 1e32e5a5154a2e765ca0b3ab8e50e4c01bbe5d18

Fix font rendering regression on macOS
This fixes a regression introduced in
77f2d6e853f1ad54e6dc844a811b78daeb463e76.

Fixes #3809.

font/src/darwin/mod.rs
@@ -515,8 +515,12 @@ impl Font {
             kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host,
         );
 
+        let is_colored = self.is_colored();
+
         // Set background color for graphics context.
-        cg_context.set_rgb_fill_color(0.0, 0.0, 0.0, 0.0);
+        let bg_a = if is_colored { 0.0 } else { 1.0 };
+        cg_context.set_rgb_fill_color(0.0, 0.0, 0.0, bg_a);
+
         let context_rect = CGRect::new(
             &CGPoint::new(0.0, 0.0),
             &CGSize::new(f64::from(rasterized_width), f64::from(rasterized_height)),
@@ -550,7 +554,7 @@ impl Font {
 
         let rasterized_pixels = cg_context.data().to_vec();
 
-        let buf = if self.is_colored() {
+        let buf = if is_colored {
             BitmapBuffer::RGBA(byte_order::extract_rgba(&rasterized_pixels))
         } else {
             BitmapBuffer::RGB(byte_order::extract_rgb(&rasterized_pixels))