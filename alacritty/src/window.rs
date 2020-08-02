f357726d0732d21a07c1bd3f4471a274820dccf4
Christian Duerr <contact@christianduerr.com>
2020-08-02 21:23:39 +0200

    Fix negative window position
    This resolves an issue where negative window positions set in the
    configuration file would not place the Alacritty window in the correct
    location.
    
    Fixes #4061.

--- alacritty/src/window.rs
+++ alacritty/src/window.rs
@@ -334,7 +334,7 @@ impl Window {
     #[cfg(windows)]
     pub fn set_urgent(&self, _is_urgent: bool) {}
 
-    pub fn set_outer_position(&self, pos: PhysicalPosition<u32>) {
+    pub fn set_outer_position(&self, pos: PhysicalPosition<i32>) {
         self.window().set_outer_position(pos);
     }
 
