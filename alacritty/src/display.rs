03bc319848e2e0b6bd59c593e43a80e3aebba64f
Christian Duerr <contact@christianduerr.com>
2020-09-25 00:56:42 +0200

    Fix unicode search IME crash

--- alacritty/src/display.rs
+++ alacritty/src/display.rs
@@ -640,10 +640,11 @@ impl Display {
 
         // Truncate beginning of the search regex if it exceeds the viewport width.
         let num_cols = size_info.cols().0;
-        let label_len = search_label.len();
-        let regex_len = formatted_regex.len();
+        let label_len = search_label.chars().count();
+        let regex_len = formatted_regex.chars().count();
         let truncate_len = min((regex_len + label_len).saturating_sub(num_cols), regex_len);
-        let truncated_regex = &formatted_regex[truncate_len..];
+        let index = formatted_regex.char_indices().nth(truncate_len).map(|(i, _c)| i).unwrap_or(0);
+        let truncated_regex = &formatted_regex[index..];
 
         // Add search label to the beginning of the search regex.
         let mut bar_text = format!("{}{}", search_label, truncated_regex);
