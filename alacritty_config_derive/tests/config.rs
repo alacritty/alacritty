8dc6887eb1a9ce1f0e3c9bd7fc76993d77d3637a
Christian Duerr <contact@christianduerr.com>
2020-12-12 00:50:54 +0100

    Fix formatting

--- alacritty_config_derive/tests/config.rs
+++ alacritty_config_derive/tests/config.rs
@@ -111,7 +111,8 @@ fn config_deserialize() {
     let error_logs = logger.error_logs.lock().unwrap();
     assert_eq!(error_logs.as_slice(), [
         "Config error: field1: invalid type: string \"testing\", expected usize",
-        "Config error: enom_error: unknown variant `HugaBuga`, expected one of `One`, `Two`, `Three`",
+        "Config error: enom_error: unknown variant `HugaBuga`, expected one of `One`, `Two`, \
+         `Three`",
     ]);
     let warn_logs = logger.warn_logs.lock().unwrap();
     assert_eq!(warn_logs.as_slice(), [
