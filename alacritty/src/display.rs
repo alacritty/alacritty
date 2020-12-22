alacritty/alacritty

commit 5725f5812ca5e8d7d992da4ac66aa454a8a9a9ad

Fix artifacts on macOS with transparent windows
Due to the way macOS draws shadows for transparent windows, resizing
them will lead to text artifacts remaining present after a window has
been resized.

The `invalidateShadow` call is used whenever the opacity isn't `1.0` to
make sure these shadows are cleared before redrawing, so no artifacts
remain when resizing transparent windows.

Fixes #889.

alacritty/src/display.rs
@@ -610,6 +610,12 @@ impl Display {
         #[cfg(all(feature = "wayland", not(any(target_os = "macos", windows))))]
         self.request_frame(&self.window);
 
+        // Clear window shadows to prevent shadow artifacts on macOS.
+        #[cfg(target_os = "macos")]
+        if config.ui_config.background_opacity() < 1.0 {
+            self.window.invalidate_shadow();
+        }
+
         self.window.swap_buffers();
 
         #[cfg(all(feature = "x11", not(any(target_os = "macos", windows))))]

alacritty/src/window.rs
@@ -31,6 +31,8 @@ use {
 
 use std::fmt::{self, Display, Formatter};
 
+#[cfg(target_os = "macos")]
+use cocoa::base::id;
 use glutin::dpi::{PhysicalPosition, PhysicalSize};
 use glutin::event_loop::EventLoop;
 #[cfg(target_os = "macos")]
@@ -41,6 +43,10 @@ use glutin::window::{
     CursorIcon, Fullscreen, UserAttentionType, Window as GlutinWindow, WindowBuilder, WindowId,
 };
 use glutin::{self, ContextBuilder, PossiblyCurrent, WindowedContext};
+#[cfg(target_os = "macos")]
+use objc::{msg_send, sel, sel_impl};
+#[cfg(target_os = "macos")]
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
 #[cfg(windows)]
 use winapi::shared::minwindef::WORD;
 
@@ -433,6 +439,19 @@ impl Window {
 
     pub fn resize(&self, size: PhysicalSize<u32>) {
         self.windowed_context.resize(size);
+    }
+
+    /// Force macOS to clear shadow of transparent windows.
+    #[cfg(target_os = "macos")]
+    pub fn invalidate_shadow(&self) {
+        let raw_window = match self.window().raw_window_handle() {
+            RawWindowHandle::MacOS(handle) => handle.ns_window as id,
+            _ => return,
+        };
+
+        unsafe {
+            let _: () = msg_send![raw_window, invalidateShadow];
+        }
     }
 
     fn window(&self) -> &GlutinWindow {

alacritty/Cargo.toml
@@ -46,6 +46,8 @@ xdg = "2"
 png = { version = "0.16.8", default-features = false, optional = true }
 
 [target.'cfg(target_os = "macos")'.dependencies]
+raw-window-handle = "0.3.3"
+cocoa = "0.24.0"
 objc = "0.2.2"
 
 [target.'cfg(not(any(target_os="windows", target_os="macos")))'.dependencies]

commit 002811fd1e822606004286ac8e03789dabf7d548

Disable shadows for transparent windows on macOS
Commit 5725f58 introduced a performance regression on macOS due to
excessive calls to the `invalidateShadow` function, however calling this
function only on redraw after a resize was performed does not fix the
underlying problem.

As a solution, window shadows are now disabled completely for all
transparent windows. This makes sure there is no performance impact,
while still solving the problem with text artifacts on resize.

Fixes #4604.

alacritty/src/display.rs
@@ -254,6 +254,10 @@ impl Display {
         #[cfg(target_os = "macos")]
         crossfont::set_font_smoothing(config.ui_config.font.use_thin_strokes);
 
+        // Disable shadows for transparent windows on macOS.
+        #[cfg(target_os = "macos")]
+        window.set_has_shadows(config.ui_config.background_opacity() >= 1.0);
+
         #[cfg(all(feature = "x11", not(any(target_os = "macos", windows))))]
         let is_x11 = event_loop.is_x11();
         #[cfg(not(any(feature = "x11", target_os = "macos", windows)))]

alacritty/src/event.rs
@@ -1279,6 +1279,14 @@ impl<N: Notify + OnResize> Processor<N> {
         #[cfg(target_os = "macos")]
         crossfont::set_font_smoothing(config.ui_config.font.use_thin_strokes);
 
+        // Disable shadows for transparent windows on macOS.
+        #[cfg(target_os = "macos")]
+        if processor.ctx.config.ui_config.background_opacity()
+            != config.ui_config.background_opacity()
+        {
+            processor.ctx.window.set_has_shadows(config.ui_config.background_opacity() >= 1.0);
+        }
+
         *processor.ctx.config = config;
 
         // Update cursor blinking.

alacritty/src/window.rs
@@ -32,7 +32,7 @@ use {
 use std::fmt::{self, Display, Formatter};
 
 #[cfg(target_os = "macos")]
-use cocoa::base::id;
+use cocoa::base::{id, NO, YES};
 use glutin::dpi::{PhysicalPosition, PhysicalSize};
 use glutin::event_loop::EventLoop;
 #[cfg(target_os = "macos")]
@@ -216,6 +216,15 @@ impl Window {
         } else {
             None
         };
+
+        // Disable window shadows on macOS to prevent artifacts with transparency.
+        #[cfg(target_os = "macos")]
+        if let RawWindowHandle::MacOS(handle) = windowed_context.window().raw_window_handle() {
+            let raw_window = handle.ns_window as id;
+            unsafe {
+                let _: () = msg_send![raw_window, setHasShadow: NO];
+            }
+        }
 
         let dpr = windowed_context.window().scale_factor();
 
@@ -441,16 +450,19 @@ impl Window {
         self.windowed_context.resize(size);
     }
 
-    /// Force macOS to clear shadow of transparent windows.
+    /// Disable macOS window shadows.
+    ///
+    /// This prevents rendering artifacts from showing up when the window is transparent.
     #[cfg(target_os = "macos")]
-    pub fn invalidate_shadow(&self) {
+    pub fn set_has_shadows(&self, has_shadows: bool) {
         let raw_window = match self.window().raw_window_handle() {
             RawWindowHandle::MacOS(handle) => handle.ns_window as id,
             _ => return,
         };
 
+        let value = if has_shadows { YES } else { NO };
         unsafe {
-            let _: () = msg_send![raw_window, invalidateShadow];
+            let _: () = msg_send![raw_window, setHasShadow: value];
         }
     }
 