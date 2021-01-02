b8d388ecdd883b4b728e5d2375497f630cc822f2
colpshift <marcos.colpani@gmail.com>
2021-01-02 01:22:50 -0300

    02/01/2021 01:22 colps

---
+++ ala_desktop_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+# File: alacritty desktop install
+# Author: Colps
+# Github: https://github.com/colpshift
+# Description: install alacritty desktop
+# Last Modified: 02/01/2021 01:11
+#
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
