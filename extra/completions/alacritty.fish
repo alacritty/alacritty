f8dcf142aa49f42d25b053077e431449486025a6
Jason <ammgws@users.noreply.github.com>
2021-01-13 19:24:32 +0900

    Update fish completions
    Flags removed in https://github.com/alacritty/alacritty/commit/07684281901b8d287221ade9b7c93a0f437a26f1

--- extra/completions/alacritty.fish
+++ extra/completions/alacritty.fish
@@ -10,15 +10,6 @@ complete -c alacritty \
 
 # Config
 complete -c alacritty \
-  -l "live-config-reload" \
-  -d "Enable automatic config reloading"
-complete -c alacritty \
-  -l "no-live-config-reload" \
-  -d "Disable automatic config reloading"
-complete -c alacritty \
-  -l "persistent-logging" \
-  -d "Keep the log file after quitting Alacritty"
-complete -c alacritty \
   -f \
   -l "config-file" \
   -d "Specify an alternative config file"
@@ -71,17 +62,6 @@ complete \
   -c alacritty \
   -l "ref-test" \
   -d "Generates ref test"
-
-complete \
-  -c alacritty \
-  -s "d" \
-  -l "dimensions" \
-  -d "Window dimensions <columns> <lines>"
-
-complete \
-  -c alacritty \
-  -l "position" \
-  -d "Window position <x-pos> <y-pos>"
 
 complete \
   -c alacritty \
