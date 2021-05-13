14a43112a528f49bb389c0f693e0935fb9379dcf
Niek Wilting <NiekWilting@hotmail.com>
2021-05-13 11:45:22 +0200

    Replace tabs with spaces when converting lines to strings

--- alacritty_terminal/src/term/mod.rs
+++ alacritty_terminal/src/term/mod.rs
@@ -400,26 +400,17 @@ impl<T> Term<T> {
             cols.start -= 1;
         }
 
-        let mut tab_mode = false;
         for column in (cols.start.0..line_length.0).map(Column::from) {
             let cell = &grid_line[column];
 
-            // Skip over cells until next tab-stop once a tab was found.
-            if tab_mode {
-                if self.tabs[column] {
-                    tab_mode = false;
-                } else {
-                    continue;
-                }
-            }
-
-            if cell.c == '\t' {
-                tab_mode = true;
-            }
-
             if !cell.flags.intersects(Flags::WIDE_CHAR_SPACER | Flags::LEADING_WIDE_CHAR_SPACER) {
-                // Push cells primary character.
-                text.push(cell.c);
+                if cell.c == '\t' {
+                    // Replace tabs with spaces.
+                    text.push(' ');
+                } else {
+                    // Push cells primary character.
+                    text.push(cell.c);
+                }
 
                 // Push zero-width characters.
                 for c in cell.zerowidth().into_iter().flatten() {
