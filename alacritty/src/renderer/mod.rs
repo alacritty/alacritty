7d36d8adaf66d11c1203d8cb621201d7d69c6d84
Christian Duerr <contact@christianduerr.com>
2021-05-20 21:27:37 +0200

    Fix startup caching of bold glyphs

--- alacritty/src/renderer/mod.rs
+++ alacritty/src/renderer/mod.rs
@@ -364,7 +364,7 @@ impl GlyphCache {
     /// Prefetch glyphs that are almost guaranteed to be loaded anyways.
     fn load_common_glyphs<L: LoadGlyph>(&mut self, loader: &mut L) {
         self.load_glyphs_for_font(self.font_key, loader);
-        self.load_glyphs_for_font(self.bold_italic_key, loader);
+        self.load_glyphs_for_font(self.bold_key, loader);
         self.load_glyphs_for_font(self.italic_key, loader);
         self.load_glyphs_for_font(self.bold_italic_key, loader);
     }
