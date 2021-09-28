a8c29094b721efd96eb543ed6cf991867d8b3798
Christian Duerr <contact@christianduerr.com>
2021-09-28 09:43:24 +0200

    Fix incorrect vi mode search origin
    Fixes #5460.

--- alacritty/src/event.rs
+++ alacritty/src/event.rs
@@ -420,6 +420,11 @@ impl<'a, N: Notify + 'a, T: EventListener> input::ActionContext<T> for ActionCon
         if self.terminal.mode().contains(TermMode::VI) {
             self.search_state.origin = self.terminal.vi_mode_cursor.point;
             self.search_state.display_offset_delta = 0;
+
+            // Adjust origin for content moving upward on search start.
+            if self.terminal.grid().cursor.point.line + 1 == self.terminal.screen_lines() {
+                self.search_state.origin.line -= 1;
+            }
         } else {
             let viewport_top = Line(-(self.terminal.grid().display_offset() as i32)) - 1;
             let viewport_bottom = viewport_top + self.terminal.bottommost_line();
