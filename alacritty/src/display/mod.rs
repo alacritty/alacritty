d73a74b560010f43a9f1463d35c5b64aec23c9c9
kumattau <kumattau@gmail.com>
2022-04-16 21:53:16 +0900

    Fix cell_height of impl From<SizeInfo<f32>> for WindowSize

--- alacritty/src/display/mod.rs
+++ alacritty/src/display/mod.rs
@@ -183,7 +183,7 @@ impl From<SizeInfo<f32>> for WindowSize {
             num_cols: size_info.columns() as u16,
             num_lines: size_info.screen_lines() as u16,
             cell_width: size_info.cell_width() as u16,
-            cell_height: size_info.cell_width() as u16,
+            cell_height: size_info.cell_height() as u16,
         }
     }
 }
