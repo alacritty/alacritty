2d619850aebedc93c3f7790d1383ef2fdae2b432
Kirill Chibisov <contact@kchibisov.com>
2022-11-28 13:12:53 +0300

    Add 'debug.renderer' config option
    This should help trouble shooting the renderer being created and
    different renderer options to determine when something like dual-source
    rendering isn't working.

--- alacritty/src/display/mod.rs
+++ alacritty/src/display/mod.rs
@@ -420,7 +420,7 @@ impl Display {
         let context = gl_context.make_current(&surface)?;
 
         // Create renderer.
-        let mut renderer = Renderer::new(&context)?;
+        let mut renderer = Renderer::new(&context, config.debug.renderer)?;
 
         // Load font common glyphs to accelerate rendering.
         debug!("Filling glyph cache with common glyphs");
