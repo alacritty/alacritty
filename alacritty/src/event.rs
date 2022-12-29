2c0a4822ca1019c649b65023d096693ee8bf20cc
Kirill Chibisov <contact@kchibisov.com>
2022-12-29 12:13:29 +0300

    Fix crash when one dimension of the window is zero
    This fixes a crash on Windows when the user resizes the window
    to the point that it has the height of zero. The crash was introduced
    by the glutin update, since it requires non-zero sizes for the
    resize.

--- alacritty/src/event.rs
+++ alacritty/src/event.rs
@@ -1179,8 +1179,11 @@ impl input::Processor<EventProxy, ActionContext<'_, Notifier, EventProxy>> {
                         // Minimizing the window sends a Resize event with zero width and
                         // height. But there's no need to ever actually resize to this.
                         // ConPTY has issues when resizing down to zero size and back.
+                        //
+                        // The user could also resize the window to have zero height
+                        // due to the titlebar.
                         #[cfg(windows)]
-                        if size.width == 0 && size.height == 0 {
+                        if size.width == 0 || size.height == 0 {
                             return;
                         }
 
