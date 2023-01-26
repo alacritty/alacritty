bbe49ef5470c2e833af26e91c61aa1909208ab93
Christian Duerr <contact@christianduerr.com>
2023-01-26 20:10:24 +0100

    create new windows with Cmd+N by default
    This changes the default Cmd+N binding on macOS to create a new window
    rather than spawning a new instance.
    
    Initially this change was held back for further testing of the
    multi-window feature. At this point all significant issues found with it
    have been fixed so it should be ready for prime-time now.

--- alacritty/src/config/bindings.rs
+++ alacritty/src/config/bindings.rs
@@ -714,7 +714,7 @@ pub fn platform_key_bindings() -> Vec<KeyBinding> {
             Action::Esc("\x0c".into());
         K, ModifiersState::LOGO, ~BindingMode::VI, ~BindingMode::SEARCH;  Action::ClearHistory;
         V, ModifiersState::LOGO, ~BindingMode::VI; Action::Paste;
-        N, ModifiersState::LOGO; Action::SpawnNewInstance;
+        N, ModifiersState::LOGO; Action::CreateNewWindow;
         F, ModifiersState::CTRL | ModifiersState::LOGO; Action::ToggleFullscreen;
         C, ModifiersState::LOGO; Action::Copy;
         C, ModifiersState::LOGO, +BindingMode::VI, ~BindingMode::SEARCH; Action::ClearSelection;
