alacritty/alacritty

commit bd4906722a1a026b01f06c94c33b13ff63a7e044

Switch to TOML configuration format
This switches Alacritty's default configuration format from yaml to
toml. While yaml is still supported, it is done by converting it to toml
and should be removed entirely in the future.

All existing features were persisted based on my testing. Behavior
should not change much, though `--option` might have slightly different
behavior since the entire line is not interpreted as one line of toml.

A new `alacritty migrate` subcommand has been added which allows
automatic migration from yaml to toml. This also could be used as a
facility to automatically fix configuration file changes in the future.

Closes #6592.

alacritty/src/window_context.rs
@@ -70,7 +70,7 @@ pub struct WindowContext {
     master_fd: RawFd,
     #[cfg(not(windows))]
     shell_pid: u32,
-    ipc_config: Vec<(String, serde_yaml::Value)>,
+    ipc_config: Vec<(String, toml::Value)>,
     config: Rc<UiConfig>,
 }
 
@@ -379,8 +379,8 @@ impl WindowContext {
                     },
                 };
 
-                // Try and parse value as yaml.
-                match serde_yaml::from_str(value) {
+                // Try and parse value as toml.
+                match toml::from_str(value) {
                     Ok(value) => self.ipc_config.push((key.to_owned(), value)),
                     Err(err) => error!(
                         target: LOG_TARGET_IPC_CONFIG,

alacritty_config/src/lib.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 
 use log::LevelFilter;
 use serde::Deserialize;
-use serde_yaml::Value;
+use toml::Value;
 
 pub trait SerdeReplace {
     fn replace(&mut self, key: &str, value: Value) -> Result<(), Box<dyn Error>>;

alacritty_config_derive/src/serde_replace.rs
@@ -28,7 +28,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 pub fn derive_direct(ident: Ident, generics: Generics) -> TokenStream2 {
     quote! {
         impl <#generics> alacritty_config::SerdeReplace for #ident <#generics> {
-            fn replace(&mut self, key: &str, value: serde_yaml::Value) -> Result<(), Box<dyn std::error::Error>> {
+            fn replace(&mut self, key: &str, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
                 if !key.is_empty() {
                     let error = format!("Fields \"{}\" do not exist", key);
                     return Err(error.into());
@@ -53,7 +53,7 @@ pub fn derive_recursive<T>(
     quote! {
         #[allow(clippy::extra_unused_lifetimes)]
         impl <'de, #constrained> alacritty_config::SerdeReplace for #ident <#unconstrained> {
-            fn replace(&mut self, key: &str, value: serde_yaml::Value) -> Result<(), Box<dyn std::error::Error>> {
+            fn replace(&mut self, key: &str, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
                 if key.is_empty() {
                     *self = serde::Deserialize::deserialize(value)?;
                     return Ok(());

alacritty_terminal/src/ansi.rs
@@ -52,11 +52,7 @@ impl<'de> serde::Deserialize<'de> for CursorShapeShim {
 }
 
 impl alacritty_config::SerdeReplace for CursorShapeShim {
-    fn replace(
-        &mut self,
-        key: &str,
-        value: serde_yaml::Value,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    fn replace(&mut self, key: &str, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
         if !key.is_empty() {
             return Err(format!("Fields \"{0}\" do not exist", key).into());
         }

alacritty_config_derive/tests/config.rs
@@ -86,20 +86,22 @@ fn config_deserialize() {
     log::set_logger(logger).unwrap();
     log::set_max_level(log::LevelFilter::Warn);
 
-    let test: Test = serde_yaml::from_str(
+    let test: Test = toml::from_str(
         r#"
-        field1: 3
-        field3: 32
-        nesting:
-          field1: "testing"
-          field2: None
-          field3: 99
-          aliased: 8
-        flatty: 123
-        enom_small: "one"
-        enom_big: "THREE"
-        enom_error: "HugaBuga"
-        gone: false
+        field1 = 3
+        field3 = 32
+
+        flatty = 123
+        enom_small = "one"
+        enom_big = "THREE"
+        enom_error = "HugaBuga"
+        gone = false
+
+        [nesting]
+        field1 = "testing"
+        field2 = "None"
+        field3 = 99
+        aliased = 8
     "#,
     )
     .unwrap();
@@ -121,15 +123,16 @@ fn config_deserialize() {
     // Verify all log messages are correct.
     let error_logs = logger.error_logs.lock().unwrap();
     assert_eq!(error_logs.as_slice(), [
-        "Config error: field1: invalid type: string \"testing\", expected usize",
         "Config error: enom_error: unknown variant `HugaBuga`, expected one of `One`, `Two`, \
          `Three`",
+        "Config error: field1: invalid type: string \"testing\", expected usize",
     ]);
     let warn_logs = logger.warn_logs.lock().unwrap();
     assert_eq!(warn_logs.as_slice(), [
         "Config warning: field1 has been deprecated; use field2 instead",
         "Config warning: enom_error has been deprecated",
         "Config warning: gone has been removed; it's gone",
+        "Unused config key: field3",
     ]);
 }
 
@@ -170,7 +173,7 @@ impl Log for Logger {
 fn field_replacement() {
     let mut test = Test::default();
 
-    let value = serde_yaml::to_value(13).unwrap();
+    let value = toml::Value::Integer(13);
     test.replace("nesting.field2", value).unwrap();
 
     assert_eq!(test.nesting.field2, Some(13));
@@ -180,7 +183,7 @@ fn field_replacement() {
 fn replace_derive() {
     let mut test = Test::default();
 
-    let value = serde_yaml::to_value(9).unwrap();
+    let value = toml::Value::Integer(9);
     test.replace("nesting.newtype", value).unwrap();
 
     assert_eq!(test.nesting.newtype, NewType(9));
@@ -190,7 +193,7 @@ fn replace_derive() {
 fn replace_flatten() {
     let mut test = Test::default();
 
-    let value = serde_yaml::to_value(7).unwrap();
+    let value = toml::Value::Integer(7);
     test.replace("flatty", value).unwrap();
 
     assert_eq!(test.flatten.flatty, 7);

.editorconfig
@@ -10,10 +10,6 @@ insert_final_newline = true
 indent_style = space
 indent_size = 4
 
-[*.yml]
-indent_style = space
-indent_size = 2
-
 [Makefile]
 indent_style = tab
 

.github/workflows/release.yml
@@ -76,6 +76,8 @@ jobs:
         run: |
           scdoc < extra/man/alacritty.1.scd | gzip -c > "./alacritty.1.gz"
           scdoc < extra/man/alacritty-msg.1.scd | gzip -c > "./alacritty-msg.1.gz"
+          scdoc < extra/man/alacritty.5.scd | gzip -c > "./alacritty.5.gz"
+          scdoc < extra/man/alacritty-bindings.5.scd | gzip -c > "./alacritty-bindings.5.gz"
       - name: Upload Assets
         run: |
           mv ./extra/logo/alacritty-term.svg ./Alacritty.svg
@@ -87,4 +89,3 @@ jobs:
           ./.github/workflows/upload_asset.sh ./extra/completions/_alacritty $GITHUB_TOKEN
           ./.github/workflows/upload_asset.sh ./extra/linux/Alacritty.desktop $GITHUB_TOKEN
           ./.github/workflows/upload_asset.sh ./extra/alacritty.info $GITHUB_TOKEN
-          ./.github/workflows/upload_asset.sh ./alacritty.yml $GITHUB_TOKEN

Makefile
@@ -4,7 +4,8 @@ ASSETS_DIR = extra
 RELEASE_DIR = target/release
 MANPAGE = $(ASSETS_DIR)/man/alacritty.1.scd
 MANPAGE-MSG = $(ASSETS_DIR)/man/alacritty-msg.1.scd
-CONFIGFILE = alacritty.yml
+MANPAGE-CONFIG = $(ASSETS_DIR)/man/alacritty.5.scd
+MANPAGE-CONFIG-BINDINGS = $(ASSETS_DIR)/man/alacritty-bindings.5.scd
 TERMINFO = $(ASSETS_DIR)/alacritty.info
 COMPLETIONS_DIR = $(ASSETS_DIR)/completions
 COMPLETIONS = $(COMPLETIONS_DIR)/_alacritty \
@@ -48,10 +49,11 @@ $(APP_NAME)-%: $(TARGET)-%
 	@mkdir -p $(APP_COMPLETIONS_DIR)
 	@scdoc < $(MANPAGE) | gzip -c > $(APP_EXTRAS_DIR)/alacritty.1.gz
 	@scdoc < $(MANPAGE-MSG) | gzip -c > $(APP_EXTRAS_DIR)/alacritty-msg.1.gz
+	@scdoc < $(MANPAGE-CONFIG) | gzip -c > $(APP_EXTRAS_DIR)/alacritty.5.gz
+	@scdoc < $(MANPAGE-CONFIG-BINDINGS) | gzip -c > $(APP_EXTRAS_DIR)/alacritty-bindings.5.gz
 	@tic -xe alacritty,alacritty-direct -o $(APP_EXTRAS_DIR) $(TERMINFO)
 	@cp -fRp $(APP_TEMPLATE) $(APP_DIR)
 	@cp -fp $(APP_BINARY) $(APP_BINARY_DIR)
-	@cp -fp $(CONFIGFILE) $(APP_EXTRAS_DIR)/
 	@cp -fp $(COMPLETIONS) $(APP_COMPLETIONS_DIR)
 	@touch -r "$(APP_BINARY)" "$(APP_DIR)/$(APP_NAME)"
 	@codesign --remove-signature "$(APP_DIR)/$(APP_NAME)"

commit f4512b0526212936880d5e147120829544195812

Fix `alacritty msg config` toml replacement
This fixes a regression introduced in bd49067 which broke the override
of configuration file variables using `alacritty msg config`.

To fix this the `replace` functionality was rewritten to behave more
like the `serde_utils::merge` where entire values are inserted into the
existing structure rather than separating the keys from the values.

alacritty/src/window_context.rs
@@ -70,7 +70,7 @@ pub struct WindowContext {
     master_fd: RawFd,
     #[cfg(not(windows))]
     shell_pid: u32,
-    ipc_config: Vec<(String, toml::Value)>,
+    ipc_config: Vec<toml::Value>,
     config: Rc<UiConfig>,
 }
 
@@ -273,13 +273,12 @@ impl WindowContext {
             // Apply each option, removing broken ones.
             let mut i = 0;
             while i < self.ipc_config.len() {
-                let (key, value) = &self.ipc_config[i];
-
-                match config.replace(key, value.clone()) {
+                let option = &self.ipc_config[i];
+                match config.replace(option.clone()) {
                     Err(err) => {
                         error!(
                             target: LOG_TARGET_IPC_CONFIG,
-                            "Unable to override option '{}': {}", key, err
+                            "Unable to override option '{}': {}", option, err
                         );
                         self.ipc_config.swap_remove(i);
                     },
@@ -367,21 +366,9 @@ impl WindowContext {
             self.ipc_config.clear();
         } else {
             for option in &ipc_config.options {
-                // Separate config key/value.
-                let (key, value) = match option.split_once('=') {
-                    Some(split) => split,
-                    None => {
-                        error!(
-                            target: LOG_TARGET_IPC_CONFIG,
-                            "'{}': IPC config option missing value", option
-                        );
-                        continue;
-                    },
-                };
-
-                // Try and parse value as toml.
-                match toml::from_str(value) {
-                    Ok(value) => self.ipc_config.push((key.to_owned(), value)),
+                // Try and parse option as toml.
+                match toml::from_str(option) {
+                    Ok(value) => self.ipc_config.push(value),
                     Err(err) => error!(
                         target: LOG_TARGET_IPC_CONFIG,
                         "'{}': Invalid IPC config value: {:?}", option, err

alacritty_config/src/lib.rs
@@ -6,15 +6,15 @@ use serde::Deserialize;
 use toml::Value;
 
 pub trait SerdeReplace {
-    fn replace(&mut self, key: &str, value: Value) -> Result<(), Box<dyn Error>>;
+    fn replace(&mut self, value: Value) -> Result<(), Box<dyn Error>>;
 }
 
 macro_rules! impl_replace {
     ($($ty:ty),*$(,)*) => {
         $(
             impl SerdeReplace for $ty {
-                fn replace(&mut self, key: &str, value: Value) -> Result<(), Box<dyn Error>> {
-                    replace_simple(self, key, value)
+                fn replace(&mut self, value: Value) -> Result<(), Box<dyn Error>> {
+                    replace_simple(self, value)
                 }
             }
         )*
@@ -35,33 +35,29 @@ impl_replace!(
 #[cfg(target_os = "macos")]
 impl_replace!(winit::platform::macos::OptionAsAlt,);
 
-fn replace_simple<'de, D>(data: &mut D, key: &str, value: Value) -> Result<(), Box<dyn Error>>
+fn replace_simple<'de, D>(data: &mut D, value: Value) -> Result<(), Box<dyn Error>>
 where
     D: Deserialize<'de>,
 {
-    if !key.is_empty() {
-        let error = format!("Fields \"{key}\" do not exist");
-        return Err(error.into());
-    }
     *data = D::deserialize(value)?;
 
     Ok(())
 }
 
 impl<'de, T: Deserialize<'de>> SerdeReplace for Vec<T> {
-    fn replace(&mut self, key: &str, value: Value) -> Result<(), Box<dyn Error>> {
-        replace_simple(self, key, value)
+    fn replace(&mut self, value: Value) -> Result<(), Box<dyn Error>> {
+        replace_simple(self, value)
     }
 }
 
 impl<'de, T: Deserialize<'de>> SerdeReplace for Option<T> {
-    fn replace(&mut self, key: &str, value: Value) -> Result<(), Box<dyn Error>> {
-        replace_simple(self, key, value)
+    fn replace(&mut self, value: Value) -> Result<(), Box<dyn Error>> {
+        replace_simple(self, value)
     }
 }
 
 impl<'de, T: Deserialize<'de>> SerdeReplace for HashMap<String, T> {
-    fn replace(&mut self, key: &str, value: Value) -> Result<(), Box<dyn Error>> {
-        replace_simple(self, key, value)
+    fn replace(&mut self, value: Value) -> Result<(), Box<dyn Error>> {
+        replace_simple(self, value)
     }
 }

alacritty_config_derive/src/serde_replace.rs
@@ -28,11 +28,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 pub fn derive_direct(ident: Ident, generics: Generics) -> TokenStream2 {
     quote! {
         impl <#generics> alacritty_config::SerdeReplace for #ident <#generics> {
-            fn replace(&mut self, key: &str, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
-                if !key.is_empty() {
-                    let error = format!("Fields \"{}\" do not exist", key);
-                    return Err(error.into());
-                }
+            fn replace(&mut self, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
                 *self = serde::Deserialize::deserialize(value)?;
 
                 Ok(())
@@ -53,19 +49,23 @@ pub fn derive_recursive<T>(
     quote! {
         #[allow(clippy::extra_unused_lifetimes)]
         impl <'de, #constrained> alacritty_config::SerdeReplace for #ident <#unconstrained> {
-            fn replace(&mut self, key: &str, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
-                if key.is_empty() {
-                    *self = serde::Deserialize::deserialize(value)?;
-                    return Ok(());
-                }
+            fn replace(&mut self, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
+                match value.as_table() {
+                    Some(table) => {
+                        for (field, next_value) in table {
+                            let next_value = next_value.clone();
+                            let value = value.clone();
 
-                let (field, next_key) = key.split_once('.').unwrap_or((key, ""));
-                match field {
-                    #replace_arms
-                    _ => {
-                        let error = format!("Field \"{}\" does not exist", field);
-                        return Err(error.into());
+                            match field.as_str() {
+                                #replace_arms
+                                _ => {
+                                    let error = format!("Field \"{}\" does not exist", field);
+                                    return Err(error.into());
+                                },
+                            }
+                        }
                     },
+                    None => *self = serde::Deserialize::deserialize(value)?,
                 }
 
                 Ok(())
@@ -95,11 +95,11 @@ fn match_arms<T>(fields: &Punctuated<Field, T>) -> TokenStream2 {
             return Error::new(ident.span(), MULTIPLE_FLATTEN_ERROR).to_compile_error();
         } else if flatten {
             flattened_arm = Some(quote! {
-                _ => alacritty_config::SerdeReplace::replace(&mut self.#ident, key, value)?,
+                _ => alacritty_config::SerdeReplace::replace(&mut self.#ident, value)?,
             });
         } else {
             stream.extend(quote! {
-                #literal => alacritty_config::SerdeReplace::replace(&mut self.#ident, next_key, value)?,
+                #literal => alacritty_config::SerdeReplace::replace(&mut self.#ident, next_value)?,
             });
         }
     }

alacritty_terminal/src/ansi.rs
@@ -52,12 +52,9 @@ impl<'de> serde::Deserialize<'de> for CursorShapeShim {
 }
 
 impl alacritty_config::SerdeReplace for CursorShapeShim {
-    fn replace(&mut self, key: &str, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
-        if !key.is_empty() {
-            return Err(format!("Fields \"{0}\" do not exist", key).into());
-        }
-
+    fn replace(&mut self, value: toml::Value) -> Result<(), Box<dyn std::error::Error>> {
         *self = serde::Deserialize::deserialize(value)?;
+
         Ok(())
     }
 }

alacritty_config_derive/tests/config.rs
@@ -173,8 +173,8 @@ impl Log for Logger {
 fn field_replacement() {
     let mut test = Test::default();
 
-    let value = toml::Value::Integer(13);
-    test.replace("nesting.field2", value).unwrap();
+    let value = toml::from_str("nesting.field2=13").unwrap();
+    test.replace(value).unwrap();
 
     assert_eq!(test.nesting.field2, Some(13));
 }
@@ -183,8 +183,8 @@ fn field_replacement() {
 fn replace_derive() {
     let mut test = Test::default();
 
-    let value = toml::Value::Integer(9);
-    test.replace("nesting.newtype", value).unwrap();
+    let value = toml::from_str("nesting.newtype=9").unwrap();
+    test.replace(value).unwrap();
 
     assert_eq!(test.nesting.newtype, NewType(9));
 }
@@ -193,8 +193,8 @@ fn replace_derive() {
 fn replace_flatten() {
     let mut test = Test::default();
 
-    let value = toml::Value::Integer(7);
-    test.replace("flatty", value).unwrap();
+    let value = toml::from_str("flatty=7").unwrap();
+    test.replace(value).unwrap();
 
     assert_eq!(test.flatten.flatty, 7);
 }