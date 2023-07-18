08db79466def16277474e4fb427aa68545710240
SonuBardai <sonubardai9@gmail.com>
2023-07-18 10:21:01 +0530

    Handle case when fg and bg are set the same

--- alacritty/src/display/content.rs
+++ alacritty/src/display/content.rs
@@ -239,15 +239,14 @@ impl RenderableCell {
             let config_fg = colors.selection.foreground;
             let config_bg = colors.selection.background;
             Self::compute_cell_rgb(&mut fg, &mut bg, &mut bg_alpha, config_fg, config_bg);
-            if content.config.colors.transparent_background_colors {
-                bg_alpha = content.config.window_opacity();
-            }
-
             if fg == bg && !cell.flags.contains(Flags::HIDDEN) {
                 // Reveal inversed text when fg/bg is the same.
                 fg = content.color(NamedColor::Background as usize);
                 bg = content.color(NamedColor::Foreground as usize);
                 bg_alpha = 1.0;
+            }
+            if content.config.colors.transparent_background_colors {
+                bg_alpha = content.config.window_opacity();
             }
         } else if content.search.as_mut().map_or(false, |search| search.advance(cell.point)) {
             let focused = content.focused_match.map_or(false, |fm| fm.contains(&cell.point));
