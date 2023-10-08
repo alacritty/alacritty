alacritty/alacritty

commit 59c63d373804fe9d00d92d2dd47174d4e1637333

Update dependencies
This patch applies all breaking and non-breaking dependency updates
and bumps MSRV to 1.70.0.

alacritty_terminal/src/tty/unix.rs
@@ -4,7 +4,8 @@ use std::ffi::CStr;
 use std::fs::File;
 use std::io::{Error, ErrorKind, Read, Result};
 use std::mem::MaybeUninit;
-use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
+use std::os::fd::OwnedFd;
+use std::os::unix::io::{AsRawFd, FromRawFd};
 use std::os::unix::net::UnixStream;
 use std::os::unix::process::CommandExt;
 use std::process::{Child, Command, Stdio};
@@ -38,7 +39,7 @@ macro_rules! die {
 }
 
 /// Get raw fds for master/slave ends of a new PTY.
-fn make_pty(size: winsize) -> Result<(RawFd, RawFd)> {
+fn make_pty(size: winsize) -> Result<(OwnedFd, OwnedFd)> {
     let mut window_size = size;
     window_size.ws_xpixel = 0;
     window_size.ws_ypixel = 0;
@@ -194,12 +195,14 @@ fn default_shell_command(shell: &str, user: &str) -> Command {
 /// Create a new TTY and return a handle to interact with it.
 pub fn new(config: &PtyConfig, window_size: WindowSize, window_id: u64) -> Result<Pty> {
     let (master, slave) = make_pty(window_size.to_winsize())?;
+    let master_fd = master.as_raw_fd();
+    let slave_fd = slave.as_raw_fd();
 
     #[cfg(any(target_os = "linux", target_os = "macos"))]
-    if let Ok(mut termios) = termios::tcgetattr(master) {
+    if let Ok(mut termios) = termios::tcgetattr(&master) {
         // Set character encoding to UTF-8.
         termios.input_flags.set(InputFlags::IUTF8, true);
-        let _ = termios::tcsetattr(master, SetArg::TCSANOW, &termios);
+        let _ = termios::tcsetattr(&master, SetArg::TCSANOW, &termios);
     }
 
     let user = ShellUser::from_env()?;
@@ -216,9 +219,9 @@ pub fn new(config: &PtyConfig, window_size: WindowSize, window_id: u64) -> Resul
     // Ownership of fd is transferred to the Stdio structs and will be closed by them at the end of
     // this scope. (It is not an issue that the fd is closed three times since File::drop ignores
     // error on libc::close.).
-    builder.stdin(unsafe { Stdio::from_raw_fd(slave) });
-    builder.stderr(unsafe { Stdio::from_raw_fd(slave) });
-    builder.stdout(unsafe { Stdio::from_raw_fd(slave) });
+    builder.stdin(unsafe { Stdio::from_raw_fd(slave_fd) });
+    builder.stderr(unsafe { Stdio::from_raw_fd(slave_fd) });
+    builder.stdout(unsafe { Stdio::from_raw_fd(slave_fd) });
 
     // Setup shell environment.
     let window_id = window_id.to_string();
@@ -237,11 +240,11 @@ pub fn new(config: &PtyConfig, window_size: WindowSize, window_id: u64) -> Resul
                 return Err(Error::new(ErrorKind::Other, "Failed to set session id"));
             }
 
-            set_controlling_terminal(slave);
+            set_controlling_terminal(slave_fd);
 
             // No longer need slave/master fds.
-            libc::close(slave);
-            libc::close(master);
+            libc::close(slave_fd);
+            libc::close(master_fd);
 
             libc::signal(libc::SIGCHLD, libc::SIG_DFL);
             libc::signal(libc::SIGHUP, libc::SIG_DFL);
@@ -274,10 +277,10 @@ pub fn new(config: &PtyConfig, window_size: WindowSize, window_id: u64) -> Resul
             unsafe {
                 // Maybe this should be done outside of this function so nonblocking
                 // isn't forced upon consumers. Although maybe it should be?
-                set_nonblocking(master);
+                set_nonblocking(master_fd);
             }
 
-            let mut pty = Pty { child, file: unsafe { File::from_raw_fd(master) }, signals };
+            let mut pty = Pty { child, file: unsafe { File::from_raw_fd(master_fd) }, signals };
             pty.on_resize(window_size);
             Ok(pty)
         },

.builds/freebsd.yml
@@ -24,8 +24,8 @@ tasks:
       cargo test
   - oldstable: |
       cd alacritty
-      rustup toolchain install --profile minimal 1.65.0
-      rustup default 1.65.0
+      rustup toolchain install --profile minimal 1.70.0
+      rustup default 1.70.0
       cargo test
   - clippy: |
       cd alacritty

.builds/linux.yml
@@ -34,8 +34,8 @@ tasks:
       cargo test
   - oldstable: |
       cd alacritty
-      rustup toolchain install --profile minimal 1.65.0
-      rustup default 1.65.0
+      rustup toolchain install --profile minimal 1.70.0
+      rustup default 1.70.0
       cargo test
   - clippy: |
       cd alacritty

.github/workflows/ci.yml
@@ -19,7 +19,7 @@ jobs:
         run: cargo test
       - name: Oldstable
         run: |
-          rustup default 1.65.0
+          rustup default 1.70.0
           cargo test
       - name: Clippy
         run: |

alacritty/Cargo.toml
@@ -7,7 +7,7 @@ description = "A fast, cross-platform, OpenGL terminal emulator"
 readme = "README.md"
 homepage = "https://github.com/alacritty/alacritty"
 edition = "2021"
-rust-version = "1.65.0"
+rust-version = "1.70.0"
 
 [dependencies.alacritty_terminal]
 path = "../alacritty_terminal"
@@ -32,13 +32,13 @@ glutin = { version = "0.30.4", default-features = false, features = ["egl", "wgl
 home = "0.5.5"
 libc = "0.2"
 log = { version = "0.4", features = ["std", "serde"] }
-notify = "5.1.0"
+notify = "6.1.1"
 once_cell = "1.12"
 parking_lot = "0.12.0"
 serde = { version = "1", features = ["derive"] }
 serde_json = "1"
-serde_yaml = "0.8"
-toml = "0.7.1"
+serde_yaml = "0.9.25"
+toml = "0.8.2"
 unicode-width = "0.1"
 winit = { version = "0.29.1-beta", default-features = false, features = ["serde"] }
 
@@ -55,12 +55,11 @@ xdg = "2.5.0"
 png = { version = "0.17.5", default-features = false, optional = true }
 
 [target.'cfg(target_os = "macos")'.dependencies]
-cocoa = "0.24.0"
+cocoa = "0.25.0"
 objc = "0.2.2"
 
 [target.'cfg(not(any(target_os="windows", target_os="macos")))'.dependencies]
 x11-dl = { version = "2", optional = true }
-wayland-client = { version = "0.29.0", features = ["dlopen"], optional = true }
 
 [target.'cfg(windows)'.dependencies]
 dirs = "5.0.1"
@@ -72,7 +71,7 @@ windows-sys = { version = "0.48", features = [
 ]}
 
 [target.'cfg(windows)'.build-dependencies]
-embed-resource = "1.7.2"
+embed-resource = "2.2.0"
 
 [features]
 default = ["wayland", "x11"]
@@ -82,12 +81,13 @@ x11 = [
     "glutin/x11",
     "glutin/glx",
     "x11-dl",
-    "png"]
+    "png",
+]
 wayland = [
     "copypasta/wayland",
     "glutin/wayland",
     "winit/wayland",
     "winit/wayland-dlopen",
     "winit/wayland-csd-adwaita-crossfont",
-    "wayland-client"]
+]
 nightly = []

alacritty/build.rs
@@ -23,7 +23,7 @@ fn main() {
     .unwrap();
 
     #[cfg(windows)]
-    embed_resource::compile("./windows/alacritty.rc");
+    embed_resource::compile("./windows/alacritty.rc", embed_resource::NONE);
 }
 
 fn commit_hash() -> Option<String> {

alacritty/src/migrate.rs
@@ -29,6 +29,7 @@ pub fn migrate(options: MigrateOptions) {
 
     // If we're doing a wet run, perform a dry run first for safety.
     if !options.dry_run {
+        #[allow(clippy::redundant_clone)]
         let mut options = options.clone();
         options.silent = true;
         options.dry_run = true;

alacritty_config/Cargo.toml
@@ -6,9 +6,9 @@ license = "MIT OR Apache-2.0"
 description = "Alacritty configuration abstractions"
 homepage = "https://github.com/alacritty/alacritty"
 edition = "2021"
-rust-version = "1.65.0"
+rust-version = "1.70.0"
 
 [dependencies]
 log = { version = "0.4.17", features = ["serde"] }
 serde = "1.0.163"
-toml = "0.7.1"
+toml = "0.8.2"

commit 3ba1c203b0374dc15a2b5881067bdf8bf4e2cae3

Fix PTY being closed on creation
The PTY got closed because `OwnedFd` automatically closed it.

Fixes: 59c63d37 (Update dependencies)

alacritty_terminal/src/tty/unix.rs
@@ -280,7 +280,7 @@ pub fn new(config: &PtyConfig, window_size: WindowSize, window_id: u64) -> Resul
                 set_nonblocking(master_fd);
             }
 
-            let mut pty = Pty { child, file: unsafe { File::from_raw_fd(master_fd) }, signals };
+            let mut pty = Pty { child, file: File::from(master), signals };
             pty.on_resize(window_size);
             Ok(pty)
         },