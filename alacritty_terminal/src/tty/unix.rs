alacritty/alacritty

commit 117719b3216d60ddde4096d9b2bdccecfd214c42

Remove extra TIOCSWINSZ ioctl on startup
The openpty call already performs it, thus no need to call it one more
with the exact same size since it confuses some applications.

alacritty_terminal/src/tty/unix.rs
@@ -280,9 +280,7 @@ pub fn new(config: &Options, window_size: WindowSize, window_id: u64) -> Result<
                 set_nonblocking(master_fd);
             }
 
-            let mut pty = Pty { child, file: File::from(master), signals };
-            pty.on_resize(window_size);
-            Ok(pty)
+            Ok(Pty { child, file: File::from(master), signals })
         },
         Err(err) => Err(Error::new(
             err.kind(),

commit 615ab7c2d1de0fbed1c073e8a62748a4a84ecc0e

Fix pty's pixelsize being zero until resize
117719b3 removed the extra call for TIOCSWINSZ, however the first call
was not setting the pixelsize for _unknown_ reason, which regressed
some clients.

Fixes: 117719b3 (Remove extra TIOCSWINSZ)

alacritty_terminal/src/tty/unix.rs
@@ -4,7 +4,6 @@ use std::ffi::CStr;
 use std::fs::File;
 use std::io::{Error, ErrorKind, Read, Result};
 use std::mem::MaybeUninit;
-use std::os::fd::OwnedFd;
 use std::os::unix::io::{AsRawFd, FromRawFd};
 use std::os::unix::net::UnixStream;
 use std::os::unix::process::CommandExt;
@@ -36,17 +35,6 @@ macro_rules! die {
         error!($($arg)*);
         std::process::exit(1);
     }}
-}
-
-/// Get raw fds for master/slave ends of a new PTY.
-fn make_pty(size: Winsize) -> Result<(OwnedFd, OwnedFd)> {
-    let mut window_size = size;
-    window_size.ws_xpixel = 0;
-    window_size.ws_ypixel = 0;
-
-    let ends = openpty(None, Some(&window_size))?;
-
-    Ok((ends.controller, ends.user))
 }
 
 /// Really only needed on BSD, but should be fine elsewhere.
@@ -194,7 +182,8 @@ fn default_shell_command(shell: &str, user: &str) -> Command {
 
 /// Create a new TTY and return a handle to interact with it.
 pub fn new(config: &Options, window_size: WindowSize, window_id: u64) -> Result<Pty> {
-    let (master, slave) = make_pty(window_size.to_winsize())?;
+    let pty = openpty(None, Some(&window_size.to_winsize()))?;
+    let (master, slave) = (pty.controller, pty.user);
     let master_fd = master.as_raw_fd();
     let slave_fd = slave.as_raw_fd();
 