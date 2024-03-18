2560474ea43c01cb4f2513e524f0df8655669076
Kirill Bulatov <mail4score@gmail.com>
2024-03-18 03:15:39 +0200

    Allow setting terminal env vars via PTY options
    Closes #7778.

--- alacritty_terminal/src/tty/mod.rs
+++ alacritty_terminal/src/tty/mod.rs
@@ -1,5 +1,6 @@
 //! TTY related functionality.
 
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::{env, io};
@@ -29,6 +30,9 @@ pub struct Options {
 
     /// Remain open after child process exits.
     pub hold: bool,
+
+    /// Extra environment variables.
+    pub env: HashMap<String, String>,
 }
 
 /// Shell options.
