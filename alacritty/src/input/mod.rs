f9cc4dcd46d4d7225b614d39d5b79431bbc28daf
William Viktorsson <williamviktorsson@gmail.com>
2024-04-17 21:15:28 +0200

    fixes #7802 to enable new windows to be created on macOS using CreateNewTab with decorations set to None

--- alacritty/src/input/mod.rs
+++ alacritty/src/input/mod.rs
@@ -36,6 +36,7 @@ use alacritty_terminal::vi_mode::ViMotion;
 use alacritty_terminal::vte::ansi::{ClearMode, Handler};
 
 use crate::clipboard::Clipboard;
+use crate::config::window::Decorations;
 use crate::config::{Action, BindingMode, MouseAction, SearchAction, UiConfig, ViAction};
 use crate::display::hint::HintMatch;
 use crate::display::window::Window;
@@ -385,8 +386,13 @@ impl<T: EventListener> Execute<T> for Action {
             Action::CreateNewWindow => ctx.create_new_window(None),
             #[cfg(target_os = "macos")]
             Action::CreateNewTab => {
-                let tabbing_id = Some(ctx.window().tabbing_id());
-                ctx.create_new_window(tabbing_id);
+                // attatching new windows as tabs only work with decorations enabled
+                if ctx.config().window.decorations == Decorations::None {
+                    ctx.create_new_window(None);
+                } else {
+                    let tabbing_id = Some(ctx.window().tabbing_id());
+                    ctx.create_new_window(tabbing_id);
+                }
             },
             #[cfg(target_os = "macos")]
             Action::SelectNextTab => ctx.window().select_next_tab(),
