alacritty/alacritty

commit 1a143d11d3296816bef1ea5b1a0b7bf54b92f4a5

Fix trigger of normal bindings in mouse mode
We should ensure that the `Shift` is actually pressed when trying to
prefer regular bindings instead of the ones if we had Shift applied.

Fixes: 500b696ca8ed (Prefer exact matches for bindings in mouse mode)
Fixes #7415.

alacritty/src/input/mod.rs
@@ -1000,7 +1000,8 @@ impl<T: EventListener, A: ActionContext<T>> Processor<T, A> {
         let mut check_fallback = mouse_mode && mods.contains(ModifiersState::SHIFT);
 
         for binding in &mouse_bindings {
-            if binding.is_triggered_by(mode, mods, &button) {
+            // Don't trigger normal bindings in mouse mode unless Shift is pressed.
+            if binding.is_triggered_by(mode, mods, &button) && (check_fallback || !mouse_mode) {
                 binding.action.execute(&mut self.ctx);
                 check_fallback = false;
             }

commit ad68a71b70d5bbd47b2328df03cb330b7fe5c26f

Fix mouse mode bindings with multiple actions
The following config was broken:
```
  [mouse]
  bindings = [
  { mouse = "Right", mods = "Shift", action = "Copy"            },
  { mouse = "Right", mods = "Shift", action = "ClearSelection"  },
  ]
```

Only the first action was applied. Change to allow more than one exact
match in mouse mode with shift held, but keep the logic to not allow
fallback search if any exact match was found.

Fixes: 1a143d11 (Fix trigger of normal bindings in mouse mode)

alacritty/src/input/mod.rs
@@ -1004,17 +1004,18 @@ impl<T: EventListener, A: ActionContext<T>> Processor<T, A> {
         let mouse_bindings = self.ctx.config().mouse_bindings().to_owned();
 
         // If mouse mode is active, also look for bindings without shift.
-        let mut check_fallback = mouse_mode && mods.contains(ModifiersState::SHIFT);
+        let fallback_allowed = mouse_mode && mods.contains(ModifiersState::SHIFT);
+        let mut exact_match_found = false;
 
         for binding in &mouse_bindings {
             // Don't trigger normal bindings in mouse mode unless Shift is pressed.
-            if binding.is_triggered_by(mode, mods, &button) && (check_fallback || !mouse_mode) {
+            if binding.is_triggered_by(mode, mods, &button) && (fallback_allowed || !mouse_mode) {
                 binding.action.execute(&mut self.ctx);
-                check_fallback = false;
+                exact_match_found = true;
             }
         }
 
-        if check_fallback {
+        if fallback_allowed && !exact_match_found {
             let fallback_mods = mods & !ModifiersState::SHIFT;
             for binding in &mouse_bindings {
                 if binding.is_triggered_by(mode, fallback_mods, &button) {