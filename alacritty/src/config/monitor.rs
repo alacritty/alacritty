0213ea9a2e9634dedd73e2d8eb371ada199c3c26
Christian Duerr <contact@christianduerr.com>
2024-07-19 02:10:27 +0200

    Use old hasher import path

--- alacritty/src/config/monitor.rs
+++ alacritty/src/config/monitor.rs
@@ -1,4 +1,5 @@
-use std::hash::{DefaultHasher, Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use std::path::PathBuf;
 use std::sync::mpsc::{self, RecvTimeoutError, Sender};
 use std::thread::JoinHandle;
